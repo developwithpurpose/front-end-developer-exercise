715c2e1f97a24426930d5ed4365e380ac7366905
Elijah Manor <elijah.manor@gmail.com>
2014-05-19 12:11:57 -0500

    Add CSSLint and update JSHint to ignore undeclared $

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,83 @@
 /*jshint -W015, -W116, -W013, -W011 */
 /*globals require:false, module:false */
 module.exports = function( grunt ) {
-    require( "matchdep" ).filterDev( "grunt-*" )
-        .forEach( grunt.loadNpmTasks );
+  require( "matchdep" ).filterDev( "grunt-*" )
+    .forEach( grunt.loadNpmTasks );
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON( "package.json" ),
-        jshint: {
-            all: [ "Gruntfile.js", "app/assets/javascripts/**/*.js", "spec/*.js" ],
-            options: {
-                jshintrc: ".jshintrc"
-            }
-        },
-        uglify: {
-            build: {
-                files: [{
-                    expand: true,
-                    cwd: "app/assets/javascripts",
-                    src: "**/*.js",
-                    dest: "build/javascripts",
-                    reset: true
-                }]
-            }
-        },
-        validation: {
-            options: {
-                stoponerror: false
-            },
-            files: {
-                src: [ "app/**/*.html" ]
-            }
-        },
-        clean: {
-            validation: [ "validation-*.json" ]
-        },
-        watch: {
-            test: {
-                files: [ "<%= jshint.all %>" ],
-                tasks: [ "uglify", "jasmine" ],
-                options: {
-                    livereload: 9000
-                }
-            },
-            lint: {
-                files: [ "<%= jshint.all %>", "app/**/*.html" ],
-                tasks: [ "jshint", "validation", "clean:validation" ]
-            }
-        },
-        jasmine: {
-            pivotal: {
-                src: "build/javascripts/**/*.js",
-                options: {
-                    specs: "spec/**/*.spec.js",
-                    vendor: [
-                        "vendor/**/*.js"
-                    ],
-                    template: "spec/index.tmpl"
-                }
-            }
-        },
-        connect: {
-            server: {
-                options: {
-                    port: 9001,
-                    livereload: true,
-                    keepalive: true
-                }
-            }
+  grunt.initConfig({
+      pkg: grunt.file.readJSON( "package.json" ),
+      jshint: {
+        all: [ "Gruntfile.js", "app/assets/javascripts/**/*.js", "spec/*.js" ],
+        options: {
+          jshintrc: ".jshintrc"
         }
-    });
+      },
+      uglify: {
+        build: {
+          files: [{
+            expand: true,
+            cwd: "app/assets/javascripts",
+            src: "**/*.js",
+            dest: "build/javascripts",
+            reset: true
+          }]
+        }
+      },
+      validation: {
+        options: {
+          stoponerror: false
+        },
+        files: {
+          src: [ "app/**/*.html" ]
+        }
+      },
+      clean: {
+        validation: [ "validation-*.json" ]
+      },
+      watch: {
+        test: {
+          files: [ "<%= jshint.all %>" ],
+          tasks: [ "uglify", "jasmine" ],
+          options: {
+            livereload: 9000
+          }
+        },
+        lint: {
+          files: [ "<%= jshint.all %>", "app/**/*.html" ],
+          tasks: [ "jshint", "validation", "clean:validation" ]
+        }
+      },
+      jasmine: {
+        pivotal: {
+          src: "build/javascripts/**/*.js",
+          options: {
+            specs: "spec/**/*.spec.js",
+            vendor: [
+              "vendor/**/*.js"
+            ],
+            template: "spec/index.tmpl"
+          }
+        }
+      },
+      connect: {
+        server: {
+          options: {
+            port: 9001,
+            livereload: true,
+            keepalive: true
+          }
+        }
+      },
+    csslint: {
+      options: {
+        csslintrc: ".csslintrc"
+      },
+      strict: {
+        src: [ "app/assets/stylesheets/**/*.css" ]
+      }
+    }
+  });
 
-    grunt.registerTask( "default", ["connect"] );
-
+  grunt.registerTask( "default", ["connect"] );
+  grunt.registerTask( "lint", ["jshint", "csslint"] );
 };
