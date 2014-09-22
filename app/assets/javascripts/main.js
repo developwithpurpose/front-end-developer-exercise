c63dffdfcdd5c28708e211fc0330d96bd3670cee
sbussard <me@stephenbussard.com>
2014-09-22 17:56:56 -0600

    Added nav sprites, updated nav transition

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,39 @@
-$(function() {
+/*
+
+Style explanation
+
+- perform jQuery selector queries in advance to avoid redundant computations
+- declare variables at beginning of each scope for readability 
+- name non-trivial functions to avoid spaghetti code
+
+*/
+
+function ready() {
 	var $navElem = $('nav > a');
 	var $navSelection = $('#navSelection');
 
-	$navElem.click(function(event) {
-		var $this = $(this);
-		var topOffset = $this.position().top + 'px';
-
-		$navElem.removeClass('selected');
-		$this.addClass('selected');
-
-		$navSelection.animate({ top: topOffset }, 200);
+	var navTransition = function(event) {
+		var $selectedElem = $(this);
+		var topOffset = $selectedElem.position().top + 'px';
+		var goAwayAndComeBack = function() {
+			$navElem.removeClass('selected');
+			$navSelection.css({ top: topOffset });
+			$navSelection.animate({ width: '300px' }, 100, function() {
+				$selectedElem.addClass('selected');
+			});
+		};
 
 		event.preventDefault();
-	});
-});
+
+		if(!$selectedElem.hasClass('selected')) {
+			$navSelection.animate({ width: '0' }, 100, goAwayAndComeBack);
+		}
+	};
+
+	$navElem.click(navTransition);
+}
+
+$(ready);
+
+
+
