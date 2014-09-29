9107fdf4711c6254c84258df9ed8a7ba28235ab2
cortney robinson <gvestudios@gmail.com>
2014-09-29 18:27:35 -0400

    dir update

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+//INCLUDE GULP
+var gulp = require('gulp');
+
+
+// ADDITIONAL REQUIRED PLUGINS
+var sass = require('gulp-sass'),
+   cssmin = require('gulp-cssmin'),
+   changed = require('gulp-changed'),
+   concat = require('gulp-concat'),
+   watch = require('gulp-watch'),
+   plumber = require('gulp-plumber'),
+   prefix = require('gulp-autoprefixer'),
+   uglify = require('gulp-uglify'),
+   browserSync = require('browser-sync'),
+   connect = require('gulp-connect');
+   livereload = require('gulp-livereload');
+
+//SET PATHS
+var paths = {
+   html    : "./app/*.html", //app html
+   sass    : "./app/assets/stylesheets/sass/**/*.scss",//source css
+   css     : "./app/assets/stylesheets/",//dest css
+   jssrc   : "./app/assets/javascripts/src/*.js",//source js
+   js      : "./app/assets/javascripts/" //des js
+}
+
+// UPDATE AND WATCH HTML
+//-----------------------------------------------
+gulp.task('html',function(){
+  return gulp.src(paths.html)
+  .pipe(changed('./'))
+  .pipe(livereload());
+});
+
+// COMPILE SASS
+//-----------------------------------------------
+gulp.task('styles',function(){
+   return gulp.src(paths.sass)
+   .pipe(plumber())
+   .pipe(sass())
+   .pipe(prefix('last 2 versions'))
+   .pipe(cssmin())
+   .pipe(gulp.dest(paths.css))
+   .pipe(livereload());
+});
+
+//Minify JS SCRIPTS
+//----------------------------------------------
+gulp.task('scripts',function(){
+   return gulp.src(paths.jssrc)
+   .pipe(uglify())
+   .pipe(concat('scripts.js'))
+   .pipe(gulp.dest(paths.js));
+});
+
+
+//Watch the file changes
+//----------------------------------------------
+gulp.task('watch',function(){
+   var server = livereload();
+  // Gulp watches for changes in CSS/JS/HTML files
+  gulp.watch(paths.sass, ['styles']);
+  gulp.watch(paths.jssrc,['scripts']);
+  gulp.watch(paths.html,['html']);
+});
+
+//CONNECT TO SERVER
+//-----------------------------------------------
+gulp.task('webserver',function(){
+   connect.server({
+      root: 'app',
+      port: 9001,
+      livereload : true
+   });
+});
+
+//Call livereload
+gulp.task('livereload',function(){
+   gulp.src([paths.sass, paths.jssrc,paths.html])
+   .pipe(watch())
+   .pipe(connect.reload());
+});
+
+
+//DEFAULT TASKS THAT GULP MUST RUN AT START
+gulp.task('default',['webserver','styles','scripts','livereload','watch']);
+
