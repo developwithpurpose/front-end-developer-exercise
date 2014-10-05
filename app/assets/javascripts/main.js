2c55cddfd75d97eb6a97859717c263381735e428
Liccy Fuentes <liccyfuentes@gmail.com>
2014-10-04 20:30:20 -0400

    a bit of clean up

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,3 @@
-/* 
- * NOTE: builded using Jquery to make faster delivery.
- */
- 
 // define a global FE object.
 var FE = FE || {};
 
@@ -18,17 +14,23 @@ FE.util = (function($) {
     return {
         init: function() {
             //listen the 'click' event for nav menu.
-            $(document).on("click", ".j-side-nav li a", function(e) {
+            $(document).on("click", "nav a", function(e) {
                 //prevent default behavior of event.
                 e.preventDefault();
+                
                 var $this = $(this),
                     step = $this.data("step");
+                
                 //if it is already active menu,we do nothing.
+				
 				if ($this.hasClass("active")){ return; }
+                
                 //transition effect.
                 FE.util.transition($this, step);
+                
                 //re-calculate the container of the active content.
                 FE.util.reCalculateContainer(step);
+                
                 //animate to show the active content.
                 $(".markering-content article").animate({
                     "marginTop": "-" + $(".main-content.step" + step).data("top") + "px"
@@ -40,71 +42,87 @@ FE.util = (function($) {
             FE.util.calculateTopData();
             FE.util.initContainer();
         },
+        
         //init 'main-content' container height for default active content.
         initContainer: function() {
             //looking for the active content.
-            var activedItem = $(".j-side-nav>li .active"),
+            var activedItem = $("nav .active"),
                 container = $("div.markering-content");
+            
             //check if can find the active content.
             if (activedItem.length > 0) {
-                var step = parseInt(activedItem.data("step"));
-                var stepContent = $(".main-content.step" + step);
+                var step = parseInt(activedItem.data("step")),
+                	stepContent = $(".main-content.step" + step);
+                
                 //set the 'main-content' height.
                 container.css({
                     height: stepContent.height()
                 });
             }
         },
+        
         //re calculate the 'markering-content' height for active content after user clicked.
         reCalculateContainer: function(step) {
             var container = $("div.markering-content"),
                 stepContent = $(".main-content.step" + step);
+            
             //set the 'markering-content' height.
             container.css({
                 height: stepContent.height()
             });
         },
+        
         //set all 'content' top data.
         calculateTopData: function() {
             var container = $("div.markering-content"),
                 containerTop = container.offset().top;
+            
             //loop the matched 'main-content' and set top data for it.
             $(".markering-content .main-content").each(function() {
                 var content = $(this);
+                
                 content.data("top", content.offset().top - containerTop);
+                
                 //if a small screen,then reduce the margin.
                 if ($(window).width() <= 600) {
                     content.addClass("re-margin");
                 }
             });
         },
+        
         //init the transition layer
         initTransition: function() {
             //find the actived nav menu.
-            var activedItem = $(".j-side-nav>li .active");
+            var activedItem = $("nav .active");
+            
             //check if can find a default actived menu.
             if (activedItem.length > 0) {
                 var s = parseInt(activedItem.data("step")) - 1,
-                    tran = $("li.transition").css({
+                    tran = $(".transition").css({
                         height: activedItem.height()
                     });
+                
                 //set init top for transition layer.
                 tran.css({
                     top: tran.height() * s
                 });
+                
                 //set the init margin top for actived article.
                 $(".markering-content article").css({
                     "marginTop": "-" + $(".main-content.step" + activedItem.data("step")).data("top") + "px"
                 });
             }
         },
+        
         //transition effect for nav menu.
         transition: function(elem, step) {
             // find the transition layer.
-            var tran = $("li.transition"),
+            var tran = $(".transition"),
                 s = parseInt(step) - 1;
+            
             //remove the active class for previous active menu.
-            $(".j-side-nav li a, .main-content").removeClass("active");
+            $("nav a, .main-content").removeClass("active");
+            
             //animate the transition effect.
             tran.show().animate({
                 top: tran.height() * s
@@ -113,6 +131,7 @@ FE.util = (function($) {
                 tran.hide();
             });
         },
+        
         //append the step content to page.
         createHTML: function(step, arr) {
             /*### Message Logic
@@ -141,6 +160,7 @@ FE.util = (function($) {
                 $(".main-content.step" + step + " .last").html("<a href='#'>" + arr[0].firstName + " " + arr[0].lastName + "</a>, <a href='#'>" + arr[1].firstName + " " + arr[1].lastName + "</a>  and " + totalNum + " other " + friend + " are also in this step");
         	}
         },
+        
         //get the step data from ajax call.
         renderData: function() {
             $.get("/app/assets/javascripts/baby-steps.json", function(data) {
