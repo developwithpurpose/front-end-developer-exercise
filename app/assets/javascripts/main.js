2b66a81eb49bcd1535b956eb382bb2b2d9e93a58
Joey Chamberlin <joey.chamberlin@gmail.com>
2014-10-30 13:51:28 -0500

    Finalized and added cross browser support

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-
   var steps = $('li'); // Array of all li's
   var currentStep = currentStep || 1; // sets current step after click or sets the first step on page load
   var clickedStep;
+  var ARTICLE_HEIGHT = 400;
+  var NAV_STEP_HEIGHT = 61;
 
 // Resets the nav styling
   var reset = function(){
@@ -10,21 +11,20 @@ $(document).ready(function(){
     $('.blue').hide();
     $('.grey').show();
   }
-
   reset();
 
 // Resets nav styles and updates selected styles and sets focus on click
-  $('.step').on('click', function() {
+  $('.step').on('click', function(event) {
+    event.preventDefault();
     clickedStep = (steps.index($(this).closest('li'))) + 1;
-    var activeImage = "assets/images/icons/individual/icons_small_bs" + clickedStep + "_blue.png"
-    var thingsAnimateDistance = (clickedStep - currentStep) * 400;
-    var navAnimateDistance = (clickedStep - currentStep) * 61;
+    var articlesAnimateDistance = (clickedStep - currentStep) * ARTICLE_HEIGHT;
+    var navAnimateDistance = (clickedStep - currentStep) * NAV_STEP_HEIGHT;
     reset();
     $(this).css('color', '#6fbee7');
     $(this).children('.grey').hide();
     $(this).children('.blue').show();
     $('#focus').animate({ "top": '+=' + navAnimateDistance + 'px' }, "slow");
-    $('#things').animate({ "top": '-=' + thingsAnimateDistance + 'px' }, "slow");
+    $('#articles').animate({ "top": '-=' + articlesAnimateDistance + 'px' }, "slow");
     currentStep = clickedStep;
   });
 
@@ -34,7 +34,6 @@ $(document).ready(function(){
     var statusHTML;
     var friends = response.friends;
     var friendDivs = $('article .friend');
-    var friendDivsLength = friendDivs.length;
 
     $.each(friendDivs, function(i, div){
       var stepFriends = [];
@@ -48,36 +47,53 @@ $(document).ready(function(){
             if (a.lastName < b.lastName) {
               return -1;
             }
-            // a must be equal to b
             return 0;
           }); // end .sort
         }
       }); // end friends .each
+
+      // set message based on number of friends in baby step
       if (stepFriends.length === 1){
         $.each(stepFriends, function(k, friend){
           statusHTML = "<p class='friend-data'><a href='#'>" + friend.firstName + " " + friend.lastName + "</a> is also in Baby Step " + friend.babyStep + "</p>";
-        });
-      } //end stepFriends .each
+        }); //end stepFriends .each
+      }
       else if (stepFriends.length === 2){
         $.each(stepFriends, function(k, friend){
           statusHTML = "<p class='friend-data'><a href='#'>" + stepFriends[0].firstName + " " + stepFriends[0].lastName + "</a> and <a href='#'>" + stepFriends[1].firstName + " " + stepFriends[1].lastName + "</a> are also in Baby Step " + friend.babyStep + ".</p>";
-        });
+        }); //end stepFriends .each
       }
       else if (stepFriends.length == 3){
         $.each(stepFriends, function(k, friend){
           statusHTML = "<p class='friend-data'><a href='#'>" + stepFriends[0].firstName + " " + stepFriends[0].lastName + "</a>, <a href='#'>" + stepFriends[1].firstName + " " + stepFriends[1].lastName + "</a> and " + (stepFriends.length - 2) + " other friend is also in Baby Step " + friend.babyStep + ".</p>";
-        });
+        }); //end stepFriends .each
       }
       else if (stepFriends.length > 3){
         $.each(stepFriends, function(k, friend){
           statusHTML = "<p class='friend-data'><a href='#'>" + stepFriends[0].firstName + " " + stepFriends[0].lastName + "</a>, <a href='#'>" + stepFriends[1].firstName + " " + stepFriends[1].lastName + "</a> and " + (stepFriends.length - 2) + " other friends are also in Baby Step " + friend.babyStep + ".</p>";
-        });
+        }); //end stepFriends .each
       }
 
       $(div).html(statusHTML);
 
     }); // end friendDivs .each
   }); // end .getJSON
+
+// Just for fun
+console.log("  _ _             _ _ _");
+console.log(" | (_)           | (_) |");
+console.log(" | |___   _____  | |_| | _____   ");
+console.log(" | | \\ \\ / / _ \\ | | | |/ / _ \\  ");
+console.log(" | | |\\ V /  __/ | | |   <  __/  ");
+console.log(" |_|_| \\_/_\\___| |_|_|_|\\_\\___|_");
+console.log(" | '_ \\ / _ \\   / _ \\| '_ \\ / _ \\");
+console.log(" | | | | (_) | | (_) | | | |  __/");
+console.log(" |_| |_|\\___/   \\___/|_| |_|\\___|");
+console.log("   ___| |___  ___");
+console.log("  / _ \\ / __|/ _ \\");
+console.log(" |  __/ \\__ \\  __/");
+console.log("  \\___|_|___/\\___|");
+
 }); // end .ready
 
 
