9807b2e90607c57b574db8aa2034060e2b295ffb
jtcooper1224 <jtcooper1224@gmail.com>
2015-01-02 14:22:01 -0500

    Look & Feel Complete
    Includes header, all content, and list items. Next step = Dynamic
    Content

--- app/assets/javascripts/bs-script.js
+++ app/assets/javascripts/bs-script.js
@@ -1,37 +1,21 @@
-$(function() {
-    var items = $('#v-nav>ul>li').each(function() {
-        $(this).click(function() {
-            //remove previous class and add it to clicked tab
-            items.removeClass('current');
-            $(this).addClass('current');
+$(function(){
+  $('#sidemenu a').on('click', function(e){
+    e.preventDefault();
 
-            //hide all content divs and show current one
-            $('#v-nav>div.tab-content').hide().eq(items.index($(this))).show('fast');
-
-            window.location.hash = $(this).attr('tab');
-        });
-    });
-
-    if (location.hash) {
-        showTab(location.hash);
+    if($(this).hasClass('open')) {
+      // do nothing because the link is already open
+    } else {
+      var oldcontent = $('#sidemenu a.open').attr('href');
+      var newcontent = $(this).attr('href');
+      
+      $(oldcontent).fadeOut('fast', function(){
+        $(newcontent).fadeIn().removeClass('hidden');
+        $(oldcontent).addClass('hidden');
+      });
+      
+     
+      $('#sidemenu a').removeClass('open');
+      $(this).addClass('open');
     }
-    else {
-        showTab("tab1");
-    }
-
-    function showTab(tab) {
-        $("#v-nav ul li:[tab*=" + tab + "]").click();
-    }
-
-    // Bind the event hashchange, using jquery-hashchange-plugin
-    $(window).hashchange(function() {
-        showTab(location.hash.replace("#", ""));
-    })
-
-    // Trigger the event hashchange on page load, using jquery-hashchange-plugin
-    $(window).hashchange();
-});
-
-$(document).ready(function(e){
-$("#v-nav .first").delay(300).fadeIn("slow");
+  });
 });
