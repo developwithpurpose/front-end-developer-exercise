18001898f08ada01f5b269082534a0f576480d8b
Spencer Sharpe <jspencersharpe@gmail.com>
2015-04-23 10:52:08 -0500

    friends appending, color correct

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -15,7 +15,21 @@ $('#bs1').on('click', function(){
     success: function(data) {
      $('.content').hide().html(data).fadeIn('slow');
     }
-  })  
+  }) 
+  $.getJSON("/app/assets/javascripts/baby-steps.json", function(data){
+    var list = [];
+    for (var i = 0; i < 5; i++) {
+      var step = data.friends[i].babyStep;
+      var last = data.friends[i].lastName;
+      var first = data.friends[i].firstName;
+      if (step === 1) {
+        list.push(first + " " + last + ", ");
+        newList = list[list.length - 1];        
+
+        $('.content').append($("<span></span>").append(newList).addClass("names")); 
+      }
+    } 
+  }) 
 });
 
 $('#bs2').on('click', function(){
@@ -25,7 +39,22 @@ $('#bs2').on('click', function(){
     success: function(data) {
      $('.content').hide().html(data).fadeIn('slow');
    }
-  })  
+  })
+  $.getJSON("/app/assets/javascripts/baby-steps.json", function(data){
+    var list = [];
+    for (var i = 0; i < 5; i++) {
+      var step = data.friends[i].babyStep;
+      var last = data.friends[i].lastName;
+      var first = data.friends[i].firstName;
+      if (step === 2) {
+        list.push(first + " " + last + ", ");
+        newList = list[list.length - 1];        
+
+        $('.content').append($("<span></span>").append(newList).addClass("names"));  
+      }
+    }   
+    $('.content').append("are on this step");    
+  })     
 });
 
 $('#bs3').on('click', function(){
@@ -36,6 +65,21 @@ $('#bs3').on('click', function(){
      $('.content').hide().html(data).fadeIn('slow');
    }
   })  
+  $.getJSON("/app/assets/javascripts/baby-steps.json", function(data){
+    var list = [];
+    for (var i = 0; i < 5; i++) {
+      var step = data.friends[i].babyStep;
+      var last = data.friends[i].lastName;
+      var first = data.friends[i].firstName;
+      if (step === 3) {
+        list.push(first + " " + last + ", ");
+        newList = list[list.length - 1];        
+
+       $('.content').append($("<span></span>").append(newList).addClass("names")); 
+      }
+    }   
+    $('.content').append("are on this step");    
+  }) 
 });
 
 $('#bs4').on('click', function(){
@@ -46,6 +90,21 @@ $('#bs4').on('click', function(){
      $('.content').hide().html(data).fadeIn('slow');
    }
   })  
+  $.getJSON("/app/assets/javascripts/baby-steps.json", function(data){
+    var list = [];
+    for (var i = 0; i < 21; i++) {
+      var step = data.friends[i].babyStep;
+      var last = data.friends[i].lastName;
+      var first = data.friends[i].firstName;
+      if (step === 4) {
+        list.push(first + " " + last + ", ");
+        newList = list[list.length - 1];        
+
+        $('.content').append($("<span></span>").append(newList).addClass("names")); 
+      }
+    }   
+    $('.content').append("are on this step");    
+  }) 
 });
 
 $('#bs5').on('click', function(){
@@ -55,6 +114,21 @@ $('#bs5').on('click', function(){
     success: function(data) {
      $('.content').hide().html(data).fadeIn('slow');
    }
+  }) 
+ $.getJSON("/app/assets/javascripts/baby-steps.json", function(data){
+    var list = [];
+    for (var i = 0; i < 21; i++) {
+      var step = data.friends[i].babyStep;
+      var last = data.friends[i].lastName;
+      var first = data.friends[i].firstName;
+      if (step === 5) {
+        list.push(first + " " + last + ", ");
+        newList = list[list.length - 1];        
+
+        $('.content').append($("<span></span>").append(newList).addClass("names")); 
+      }
+    }   
+    $('.content').append("are on this step");    
   })  
 });
 
@@ -66,6 +140,21 @@ $('#bs6').on('click', function(){
      $('.content').hide().html(data).fadeIn('slow');
    }
   })  
+  $.getJSON("/app/assets/javascripts/baby-steps.json", function(data){
+    var list = [];
+    for (var i = 0; i < 21; i++) {
+      var step = data.friends[i].babyStep;
+      var last = data.friends[i].lastName;
+      var first = data.friends[i].firstName;
+      if (step === 6) {
+        list.push(first + " " + last + ", ");
+        newList = list[list.length - 1];        
+
+        $('.content').append($("<span></span>").append(newList).addClass("names")); 
+      }
+    }   
+    $('.content').append("are on this step");    
+  }) 
 });
 
 $('#bs7').on('click', function(){
@@ -76,4 +165,21 @@ $('#bs7').on('click', function(){
      $('.content').hide().html(data).fadeIn('slow');
    }
   })  
+  $.getJSON("/app/assets/javascripts/baby-steps.json", function(data){
+    var list = [];
+    for (var i = 0; i < 21; i++) {
+      var step = data.friends[i].babyStep;
+      var last = data.friends[i].lastName;
+      var first = data.friends[i].firstName;
+      if (step === 5) {
+        list.push(first + " " + last + ", ");
+        newList = list[list.length - 1];        
+
+        $('.content').append($("<span></span>").append(newList).addClass("names")); 
+      }
+    }   
+    $('.content').append("are on this step");    
+  }) 
 });
+
+
