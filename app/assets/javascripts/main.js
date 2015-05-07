8cd04ca8de5cc22557fb778c5b539b29096cab3f
alexmacarthur <alex@macarthur.me>
2015-05-06 23:36:39 -0500

    Still fiddling with JSON call.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -16,27 +16,60 @@ $(document).ready(function(){
 		});
 
 	$.getJSON( "http://localhost:9001/app/assets/javascripts/baby-steps.json", function( data ) {
+		var othersSwitch = false;
+		var babyStepCount1 = 0,
+			babyStepCount2 = 0,
+			babyStepCount3 = 0,
+			babyStepCount4 = 0,
+			babyStepCount5 = 0,
+			babyStepCount6 = 0,
+			babyStepCount7 = 0;
 		for (var i = 0; i < data.friends.length; i++) {
 			
-			if(data.friends[i].babyStep == (i+1)){
-				$('.friends-list-' + (i+1)).append(data.friends[i].firstName);
+			switch (data.friends[i].babyStep) {
+				case 1:
+					var theBabyStep = 1;
+					++babyStepCount1;
+					break; 
+				case 2:
+					var theBabyStep = 2;
+					++babyStepCount2;
+					break; 
+				case 3:
+					var theBabyStep = 3;
+					++babyStepCount3;
+					break;
+				case 4:
+					var theBabyStep = 4;
+					++babyStepCount4;
+					break;
+				case 5:
+					var theBabyStep = 5;
+					++babyStepCount5;
+					break;
+				case 6:
+					var theBabyStep = 6;
+					++babyStepCount6;
+					break;
+				case 7:
+					var theBabyStep = 7;
+					++babyStepCount7;
+					break;
+				default: 
+					return false;
 			}
-					
-		switch (data.friends[i].babyStep) {
-			case 1:
-				var varBabyStep = 1;
-				alert(varBabyStep);
-				break; 
-			case 2:
-				var varBabyStep = 2;
-				alert(varBabyStep);
-				break; 
-			default: 
-				return true;
+
+			// if(babyStepCount7 < 2){
+			// 	// appends each name within <span> tags so :after pseduo element can add commas except for on the last child 
+			// 	$('.friends-list-' + theBabyStep).append('<span>' + data.friends[i].firstName + " " + data.friends[i].lastName + '</span>');
+			// };
+
+			// if(babyStepCount7 > 2 && othersSwitch === false){
+			// 	$('.friends-list-' + theBabyStep).append('<p>and ' + (2) + ' Other Friends are on this step.</p>');
+			// 	var othersSwitch = true;
+			// };
+
 		}
 		
-		$('.friends-list-' + varBabyStep).append(data.friends[i].firstName);
-			
-		}
 	});
 })
