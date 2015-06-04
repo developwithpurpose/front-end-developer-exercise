753ae383a35e9b9459f00fba73e5ecd0539fb177
Charles Lueker <charleslueker@yahoo.com>
2015-06-04 13:25:23 -0500

    selecting tab 2 changes everything you want it to. Now to add that functionality to all other tabs and add the friends element too

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,18 +3,31 @@ function onReady() {
 "use strict";
 
 $("#step-2").click(function changeStyle(){
+	//this changes the styling of the first tab
 	var oldSrc = "assets/images/icons/individual/icons_small_bs1_blue.png";
 	var newSrc = "assets/images/icons/individual/icons_small_bs1.png";
-	$(".chosen").addClass('baby-step').removeClass('chosen');
+	$(".first-chosen").addClass('baby-step').removeClass('first-chosen');
 	$('#step-1 img[src="' + oldSrc + '"]').attr('src', newSrc);
 	$(this).addClass('chosen').removeClass('baby-step');
-	console.log("Youve clicked step 2");
 	var list = $('#list');
 	for (var i=0; i<list.length; i++){
-		console.log('#list');//also try innerText
+		console.log(list[i]);
+		for(var i=0; i<list.length; i++){
+			console.log(this);//gets the clicked element
+			console.log(this.innerHTML);
+		var oldSrc = "assets/images/icons/individual/icons_small_bs2.png";
+		var newSrc = "assets/images/icons/individual/icons_small_bs2_blue.png";
+		$('img[src="' + oldSrc + '"]').attr('src', newSrc);
+		};
 	}
-
-});
+	//this is to change the title
+	$("#title").css({"height":"0"});
+	$("#title2").css({"height":"5em"});
+	//this changes the text of #content
+	$("#content p").html("List your debts, excluding the house, in order. The smallest balance should be your number one priority. Dont worry about interest rates unless two debts have similar payoffs. If thats the case, then list the higher interest rate debt first."+
+		"<br>"+"<br>"+
+		"The point of the debt snowball is imply this: You need some quick wins in order to stay pumped about getting out of debt! Paying off debt is not always about math. It’s about motivation. Personal finance is 20% head nowledge and 80% behavior. When you start knocking off the easier debts, you will see results and you will stay motivated to dump your debt.");
+	});
 
 $(".step-3").click(function changeStyle(){
 	console.log("Youve clicked step 3");	
