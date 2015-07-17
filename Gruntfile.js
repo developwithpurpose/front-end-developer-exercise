a2ad080812874d50e5529160bd539e36f728a9fc
Jonathan Davidson <jdavidson@netsplasher.com>
2015-07-16 22:44:30 -0500

    Gruntfile cleanup and add build task Move index.html to build directory in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,69 +5,69 @@ module.exports = function( grunt ) {
     .forEach( grunt.loadNpmTasks );
 
   grunt.initConfig({
-      pkg: grunt.file.readJSON( "package.json" ),
-      jshint: {
-        all: [ "Gruntfile.js", "app/assets/javascripts/**/*.js", "spec/*.js" ],
+    pkg: grunt.file.readJSON( "package.json" ),
+    jshint: {
+      all: [ "Gruntfile.js", "app/assets/javascripts/**/*.js", "spec/*.js" ],
+      options: {
+        jshintrc: ".jshintrc"
+      }
+    },
+    uglify: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "app/assets/javascripts",
+          src: "**/*.js",
+          dest: "build/javascripts",
+          reset: true
+        }]
+      }
+    },
+    validation: {
+      options: {
+        stoponerror: false
+      },
+      files: {
+        src: [ "app/**/*.html" ]
+      }
+    },
+    clean: {
+      validation: [ "validation-*.json" ]
+    },
+    watch: {
+      test: {
+        files: [ "<%= jshint.all %>" ],
+        tasks: [ "uglify", "jasmine" ],
         options: {
-          jshintrc: ".jshintrc"
+          livereload: 9000
         }
       },
-      uglify: {
-        build: {
-          files: [{
-            expand: true,
-            cwd: "app/assets/javascripts",
-            src: "**/*.js",
-            dest: "build/javascripts",
-            reset: true
-          }]
-        }
-      },
-      validation: {
+      lint: {
+        files: [ "<%= jshint.all %>", "<%= csslint.strict.src %>", "app/**/*.html" ],
+        tasks: [ "jshint", "csslint", "validation", "clean:validation" ]
+      }
+    },
+    jasmine: {
+      pivotal: {
+        src: "build/javascripts/**/*.js",
         options: {
-          stoponerror: false
-        },
-        files: {
-          src: [ "app/**/*.html" ]
+          specs: "spec/**/*.spec.js",
+          vendor: [
+            "vendor/**/*.js"
+          ],
+          template: "spec/index.tmpl"
         }
-      },
-      clean: {
-        validation: [ "validation-*.json" ]
-      },
-      watch: {
-        test: {
-          files: [ "<%= jshint.all %>" ],
-          tasks: [ "uglify", "jasmine" ],
-          options: {
-            livereload: 9000
-          }
-        },
-        lint: {
-          files: [ "<%= jshint.all %>", "<%= csslint.strict.src %>", "app/**/*.html" ],
-          tasks: [ "jshint", "csslint", "validation", "clean:validation" ]
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          livereload: true,
+          keepalive: true
         }
-      },
-      jasmine: {
-        pivotal: {
-          src: "build/javascripts/**/*.js",
-          options: {
-            specs: "spec/**/*.spec.js",
-            vendor: [
-              "vendor/**/*.js"
-            ],
-            template: "spec/index.tmpl"
-          }
-        }
-      },
-      connect: {
-        server: {
-          options: {
-            port: 9001,
-            livereload: true,
-            keepalive: true
-          }
-        }
-      },
+      }
+    },
     csslint: {
       options: {
         csslintrc: ".csslintrc"
@@ -75,9 +75,26 @@ module.exports = function( grunt ) {
       strict: {
         src: [ "app/assets/stylesheets/**/*.css" ]
       }
+    },
+    sass: {
+      options: {
+        sourceMap: false,
+      },
+      dist: {
+        files: {
+          "build/stylesheets/main.css": "app/assets/stylesheets/main.scss"
+        }
+      }
+    },
+    copy: {
+      main: {
+        src: "app/index.html",
+        dest: "build/index.html"
+      }
     }
   });
 
   grunt.registerTask( "default", ["connect"] );
   grunt.registerTask( "lint", ["jshint", "csslint"] );
+  grunt.registerTask( "build", ["uglify", "sass", "copy"]);
 };
