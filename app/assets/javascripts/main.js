7fbb78d64d91e09eff9b826d25f4113f00414b65
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-07-22 00:54:21 -0500

    vertical nav now working

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,32 @@
+$(function() {
+
+  var steps = $("#content>ul>li").each(function() {
+    $(this).click(function() {
+      steps.removeClass("step");
+      $(this).addClass("step");
+
+      $("#content>div.step-content").hide().eq(steps.index($(this))).show("ßfast");
+
+      window.location.hash = $(this).attr("tab");
+    });
+  });
+
+  if (location.hash) {
+        showTab(location.hash);
+    }
+    else {
+        showTab("tab1");
+    }
+
+    function showTab(tab) {
+        $("#content.step-content ul li:[tab*=" + tab + "]").click();
+    }
+
+    // Bind the event hashchange, using jquery-hashchange-plugin
+    $(window).hashchange(function() {
+        showTab(location.hash.replace("#", ""));
+    })
+
+    // Trigger the event hashchange on page load, using jquery-hashchange-plugin
+    $(window).hashchange();
+});
