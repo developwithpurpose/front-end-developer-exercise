d346f05b053fa7182578fd234a1638208fabc006
Jonathan Davidson <jcdavidson@softlayer.com>
2015-07-29 13:20:00 -0500

    Added friend item renderer

--- app/assets/javascripts/views/friend-item.js
+++ app/assets/javascripts/views/friend-item.js
@@ -8,7 +8,7 @@ export default class extends View {
         this.babyStepModel = babyStepModel;
         this.friendsCollection = friendsCollection;
 
-        this.$el = $( '#content .step' + babyStepModel.get( 'step' ) + ' .friend-view' );
+        this.$el = $( '#content .step' + babyStepModel.get( 'step' ) + ' .friends-view' );
         this.addEvents();
     }
 
@@ -22,6 +22,46 @@ export default class extends View {
         let friends = this.friendsCollection.filter( ( friend ) => {
             return friend.get( 'babyStep' ) === babyStep;
         });
+
+        this.$el.html( this.getHtml( friends, babyStep ) );
+    }
+
+    getFriendLink( friend ) {
+        const name = friend.get( 'firstName' ) + friend.get( 'lastName' );
+        const html = '<a href="javascript:void()">' + name + '</a>';
+
+        return html;
+    }
+
+    getHtml( friends, babyStep ) {
+        let html = '';
+        let content = '';
+
+        if ( 0 === friends.length ) {
+            return html;
+        }
+
+        let firstFriendLink = this.getFriendLink( friends[0] );
+
+        switch ( friends.length ) {
+            case 1:
+                content += firstFriendLink + ' is';
+                break;
+            case 2:
+                content += firstFriendLink + ' and ' + this.getFriendLink( friends[1] ) + ' are';
+                break;
+            case 3:
+                content += firstFriendLink + ', ' + this.getFriendLink( friends[1] ) + ', and 1 other friend are'
+                break;
+            default:
+                content += firstFriendLink + ', ' + this.getFriendLink( friends[1] ) + ', and ' + (friends.length - 2) + ' other friends are'
+        }
+
+        content += ' also in Baby Step ' + babyStep;
+
+        html = '<p class="content-footer">' + content + '</p>';
+
+        return html;
     }
 
 }
