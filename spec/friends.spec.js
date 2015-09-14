70b5af2e9787c0ddfd96752db757ffd4435aca00
Adam Williams <awilli1186@gmail.com>
2015-09-13 22:14:42 -0500

    removed a couple of unused files

--- spec/friends.spec.js
+++
@@ -1,9 +0,0 @@
-/*jshint esnext: true */
-
-describe("getFriends", function() {
-  var friend = "";
-    
-  it("should return friends", function() {
-    expect(friend).toEqual("Adam Williams");
-  });
-});
