2f5be69a771e2ed5ebb33f2cc0f700afb121a6c8
Rickey Messick <rmessick71@gmail.com>
2015-09-18 08:47:52 -0500

    add js comments

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,30 +4,28 @@ $(document).ready(function() {
     var $el, topPos,
         $mainNav = $("#steps-menu");
     
-    
+    //append magic line
     $mainNav.append("<li id='menu-hover-bg'></li>");
     
     var $magicLine = $("#menu-hover-bg");
     
+    // set magic line to height and width of first baby step.
     $magicLine
         .width($("#steps-menu li:first-child").parent().width())
         .height($("#steps-menu li:first-child").height())
-        .css("top", $("#steps-menu li:first-childa").position.top)
-        .data("origTop", $("#steps-menu li:first-child a").position.top)
-        .data("origWidth", $magicLine.width());
+        .css("top", $("#steps-menu li:first-child a").position.top)
                 
+    // click menu
     $("#steps-menu a").click(function(e) {
     	e.preventDefault();
 
         $el = $(this);
-
         topPos = $el.position().top;
 
+        // move magic line to new click a
         $magicLine.stop().animate({
-
             top: topPos,
             backgroundColor: "#fff"
-
         });
 
         // remove all curent page class
@@ -36,10 +34,13 @@ $(document).ready(function() {
         // add current page class to this href
         $el.addClass("current_page_item");
 
-
+        //get rel from link - matches to id of article
         var step = $(this).attr("rel");
 
+        // hide all articles
     	$(".steps-content").hide();
+
+    	// add # to clicked rel id and fadein
     	$("#" + step).fadeIn();
 
     });
