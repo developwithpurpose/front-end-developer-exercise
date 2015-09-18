fd696a7a91307d9c66c4a920569a8bb31e704255
Rickey Messick <rmessick71@gmail.com>
2015-09-18 08:40:37 -0500

    add content fadein

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -16,15 +16,32 @@ $(document).ready(function() {
         .data("origTop", $("#steps-menu li:first-child a").position.top)
         .data("origWidth", $magicLine.width());
                 
-    $("#steps-menu a").click(function() {
+    $("#steps-menu a").click(function(e) {
+    	e.preventDefault();
+
         $el = $(this);
+
         topPos = $el.position().top;
+
         $magicLine.stop().animate({
+
             top: topPos,
             backgroundColor: "#fff"
+
         });
+
+        // remove all curent page class
         $("#steps-menu a").removeClass("current_page_item");
+
+        // add current page class to this href
         $el.addClass("current_page_item");
+
+
+        var step = $(this).attr("rel");
+
+    	$(".steps-content").hide();
+    	$("#" + step).fadeIn();
+
     });
     
     // Kick IE into gear
