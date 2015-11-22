6d52aa94a72df3dfc90a619e23b95015f98971c4
david biagi <biagidp@gmail.com>
2015-11-22 09:57:30 -0600

    added the animation for the content area

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,21 +4,24 @@ $(document).ready(function(){
   $('.activeBackground').width('300px');
 
   $('.navbarItem').on('click', function(e){
-    var $element = $(this);
-    //stop default behavior
+    var $navbarElement = $(this);
+    var $contentElement = $($(this).parent().attr('href'));
+    var $previousContentElement = $($('.activeBackground').parent().parent().attr('href'));
     e.preventDefault();
-    //remove the active class from any other navbarItem
-    $('.navbarItem').removeClass('active');
-    //Add active class to this navbarItem
-    $element.addClass('active');
-    //animate any activeBackground elements closed
-    $('.activeBackground').animate({width: 0}, function(){
-      $(this).remove();
-    });
-    $element.append('<div class="activeBackground"></div>');
-    $('.activeBackground').animate({width: 300});
-    
-    $('.contentItem').hide();
-    $($(this).parent().attr('href')).show();;
+    if(!$navbarElement.hasClass('active')){
+      //remove the active class from any other navbarItem
+      $('.navbarItem').removeClass('active');
+      //Add active class to this navbarItem
+      $navbarElement.addClass('active');
+      //animate any activeBackground elements closed
+      $('.activeBackground').animate({width: 0}, function(){
+        $(this).remove();
+        $navbarElement.append('<div class="activeBackground"></div>');
+        $('.activeBackground').animate({width: 300});
+      });
+      $previousContentElement.toggle('slide', {direction: 'right'}, function(){
+        $contentElement.toggle('slide', {direction: 'right'});
+      });
+    }
   });
 });
