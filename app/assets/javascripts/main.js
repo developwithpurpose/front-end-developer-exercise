14834ef1b5a400ddcdf1c94c36ccf08d76857c66
david biagi <biagidp@gmail.com>
2015-11-22 00:10:33 -0600

    Added the animation on the navbar

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
   $('.contentItem').hide();
   $('#babyStep1').show();
+  $('.activeBackground').width('300px');
+
   $('.navbarItem').on('click', function(e){
+    var $element = $(this);
+    //stop default behavior
     e.preventDefault();
+    //remove the active class from any other navbarItem
     $('.navbarItem').removeClass('active');
-    $(this).addClass('active');
+    //Add active class to this navbarItem
+    $element.addClass('active');
+    //animate any activeBackground elements closed
+    $('.activeBackground').animate({width: 0}, function(){
+      $(this).remove();
+    });
+    $element.append('<div class="activeBackground"></div>');
+    $('.activeBackground').animate({width: 300});
+    
     $('.contentItem').hide();
     $($(this).parent().attr('href')).show();;
   });
