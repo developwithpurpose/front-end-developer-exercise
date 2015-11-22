a0f717cc7f2c18c7177a42a185f62f884e913d62
david biagi <biagidp@gmail.com>
2015-11-21 19:40:14 -0600

    Finished navigation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-  var $mainContent = $('#mainContent');
-  $mainContent.html('');
-
+  $('.contentItem').hide();
+  $('#babyStep1').show();
   $('.navbarItem').on('click', function(e){
     e.preventDefault();
     $('.navbarItem').removeClass('active');
     $(this).addClass('active');
+    $('.contentItem').hide();
+    $($(this).parent().attr('href')).show();;
   });
 });
