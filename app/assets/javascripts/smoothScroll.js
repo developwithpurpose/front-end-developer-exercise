7a9416f380a9c07921cf7fd6ce9d9fc294c84ee4
Shawn Cothran <asliceofpizza@gmail.com>
2016-02-02 19:55:46 -0600

    changesV1

---
+++ app/assets/javascripts/smoothScroll.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('.babySteps').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
