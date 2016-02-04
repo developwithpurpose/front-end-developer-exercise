4e206265796090d59d4e3ab0017416f9ca4ff234
Shawn Cothran <asliceofpizza@gmail.com>
2016-02-04 10:50:24 -0600

    everything but ajax logic

--- app/assets/javascripts/smoothScroll.js
+++ app/assets/javascripts/smoothScroll.js
@@ -8,8 +8,18 @@ $(function() {
         $('.babySteps').animate({
           scrollTop: target.position().top
         }, 720);
+        $(".slider").animate({
+          top: $(this).position().top,
+          height: $(this).height()
+        }, 720);
         return false;
       }
     }
   });
+});
+$('a[href*="#"]:not([href="#"])').click(function() {
+  $("[href]").each(function() {
+    $(this).removeClass("active");
+  });
+  $(this).addClass("active");
 });
