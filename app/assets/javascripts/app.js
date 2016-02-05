42800b7b847c68445310b9e2225570add2ef94ce
Shawn Cothran <asliceofpizza@gmail.com>
2016-02-05 00:02:13 -0600

    logic complete

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -18,6 +18,7 @@ $(function() {
   });
 });
 
+
 $('a[href*="#"]:not([href="#"])').click(function() {
   $("[href]").each(function() {
     $(this).removeClass("active");
@@ -25,24 +26,55 @@ $('a[href*="#"]:not([href="#"])').click(function() {
   $(this).addClass("active");
 });
 
+
 $.ajax('../../baby-steps.json').done(function(friendList){
   var data = friendList.friends
-
-  var sorted = _.chain(data)
+    , stepped = _.chain(data)
                 .groupBy("babyStep")
                 .pairs()
                 .map(function (currentItem) {
                   return _.object(_.zip(["babyStep", "friends"], currentItem));
                 })
                 .value();
-  console.log(sorted);
 
-  sorted.forEach(function(current, index, array){
-    if (babyStep == ) {
-  
+  $('.babyStep').each(function(){
+    var id = $(this)
+             .attr('id')
+             .slice(-1)
+      , myObject = [];
+
+    if (stepped[id-2]) {
+      $.each(stepped[id-2].friends, function(index, element){
+
+        myObject.push({
+          last : element.lastName,
+          first : element.firstName
+        })
+      })
+    };
+
+    myObject = myObject.sort(function (a, b) {
+      return a.last.localeCompare( b.last );
+    })
+
+    if (myObject.length == 1) {
+      $(this).append(
+        `<p class='friends'><a href='#'>${ myObject[0].first } ${ myObject[0].last }</a> is also in Baby Step ${ id }</p>`
+      );
+    } else if (myObject.length == 2) {
+      $(this).append(
+        `<p class='friends'><a href='#'>${ myObject[0].first } ${ myObject[0].last }</a> and <a href='#'>${ myObject[1].first } ${ myObject[1].last }</a> are also in Baby Step ${ id }</p>`
+      );
+    } else if (myObject.length == 3) {
+      $(this).append(
+        `<p class='friends'><a href='#'>${ myObject[0].first } ${ myObject[0].last }</a>, <a href='#'>${ myObject[1].first } ${ myObject[1].last }</a>, and 1 Other Friend are also in Baby Step ${ id }</p>`
+      );
+    } else if (myObject.length >= 3) {
+      $(this).append(
+        `<p class='friends'><a href='#'>${ myObject[0].first } ${ myObject[0].last }</a>, <a href='#'>${ myObject[1].first } ${ myObject[1].last }</a>, and ${ myObject.length-2 } Other Friends are also in Baby Step ${ id }</p>`
+      );
     }
-    $('.babyStep').append(`<p>${ arr.length }</p>`
-      `<p class='friends'<a href='#'>${ arr }</a> and ${ also } are on this step</p>`
-    );
-  })
+
+  });
+
 });
