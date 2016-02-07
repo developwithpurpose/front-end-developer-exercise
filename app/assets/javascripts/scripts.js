0858e4cb09885754b6fdfa8c0c312b28effe596c
mlopez99 <mlopez.coventryah@gmail.com>
2016-02-07 12:03:53 -0500

    Fixed Sprites
    Added a new sprite image to reduce file size and limit http requests.
    Updated index.html to use <span> elements for sprites to improve the
    layout. Added blank scripts.js and added reference in index.html

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,7 @@
+(function() {
+  //do stuff
+}());
+
+//remove no-js class from body
+//when user clicks li, activate link so user doesn't have to peg the link text
+//when user clicks li, prevent default, fade content out, scroll to link, fade content in
