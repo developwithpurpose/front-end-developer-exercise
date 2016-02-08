e03a41177eab73938dc2db4fdbde9b12ffcc9d97
mlopez99 <mlopez.coventryah@gmail.com>
2016-02-07 22:10:49 -0500

    Added click handler for tab links

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,7 +1,53 @@
-(function() {
-  //do stuff
-}());
+(function($, window, document) {
+  "use strict";
 
-//remove no-js class from body
-//when user clicks li, activate link so user doesn't have to peg the link text
-//when user clicks li, prevent default, fade content out, scroll to link, fade content in
+  //document ready
+  $(function() {
+    $('body').removeClass('no-js');
+  });
+
+  //tabs-nav click listener
+  $('.tabs-nav').on('click', 'li', function(e) {
+    var $tabsBody = $('.tabs-body');
+    var $this = $(this);
+    var href = $this.children().first('a').attr('href');
+
+    //animate tabbed content transitions
+    if ( !isElementInViewport($(href)) ) {
+      $tabsBody.animate({
+        opacity: 0
+      }, 150, function() {
+        $(href)[0].scrollIntoView();
+        $tabsBody.animate({
+          opacity: 1
+        }, 150);
+      }); 
+    }
+
+    //update active nav li 
+    if ( !$this.hasClass('active') ) {
+      $('.tabs-nav').find('li').removeClass('active');
+      $('.tabs-link-icon').removeClass('blue');
+      $this.addClass('active');
+      $this.children('.tabs-link-icon').addClass('blue');
+    }
+
+    e.preventDefault();
+  });
+
+  //https://gist.github.com/davidtheclark/5515733
+  function isElementInViewport (el) {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+      el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+}(window.jQuery, window, document));
