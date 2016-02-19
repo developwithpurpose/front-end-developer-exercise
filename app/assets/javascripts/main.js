8532de802d36f34a5bf22c526233531068cc358f
Jeremy <jeremygrondahl@gmail.com>
2016-02-19 10:38:23 -0600

    Added id selectors to each nav step. Implemented a click function to extract data from json file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,49 @@
 
 $(document).ready(function() {
   var counter = 0;
+  var friendsBabyStep;
+  var steps;
   $.ajax({
     url: "/../../baby-steps.json"
   }).done(function(baby_steps) {
-    for (var i = 0; i < baby_steps.friends.length; i++) {
-      var friendsOnTheSameBS = "<div>";
-      friendsOnTheSameBS +=   "<p>" + baby_steps.friends[i].firstName + " " + baby_steps.friends[i].lastName;
-      friendsOnTheSameBS += " and " + counter + " other friends are on this step.</p>";
-      friendsOnTheSameBS += "<div>";
+    $('#step1').click(function() {
+      for (var i = 0; i < baby_steps.friends.length; i++) {
+        friendsBabyStep = "<div>";
+        friendsBabyStep +=   "<p class='friends'>" + baby_steps.friends[i].firstName + " " + baby_steps.friends[i].lastName + "</p>";
+        friendsBabyStep += "<p>and " + counter + " Other Friends are on this step.</p>";
+        friendsBabyStep += "<div>";
+        steps = baby_steps.friends[i].babyStep;
       
-      if (baby_steps.friends[i].babyStep === 7) {
-        $( "#right-col" ).append(friendsOnTheSameBS);
-      }
-    }
+        // if (baby_steps.friends[i].babyStep === 7) {
+        //   $( "#myFriends" ).append(friendsBabyStep);
+        // } else if (baby_steps.friends[i].babyStep === 6) {
+        //   $( "#myFriends" ).append(friendsBabyStep);
+        // }else if (baby_steps.friends[i].babyStep === 5) {
+        //   $( "#myFriends" ).append(friendsBabyStep);
+        // }else if (baby_steps.friends[i].babyStep === 4) {
+        //   $( "#myFriends" ).append(friendsBabyStep);
+        // }else if (baby_steps.friends[i].babyStep === 3) {
+        //   $( "#myFriends" ).append(friendsBabyStep);
+        // }else if (baby_steps.friends[i].babyStep === 2) {
+        //   $( "#myFriends" ).append(friendsBabyStep);
+        // }else if (baby_steps.friends[i].babyStep === 1) {
+        //   $( "#myFriends" ).append(friendsBabyStep);
+        // } else {     
+        // }
+        if (steps === 1) {
+          $( "#myFriends" ).append(friendsBabyStep);
+        }
+      }  
+    });
+
   });
+
+
+  // $('.babyStepsNav a').click(function(){
+  //   console.log("hello");
+  //   $('.highlight').removeClass('highlight');
+  //   $(this).addClass('highlight');
+
+  // });
+
 });
