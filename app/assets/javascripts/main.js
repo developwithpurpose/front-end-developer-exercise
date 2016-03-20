3db65d3f2c6bbbb8e5896dc5861bfa3e90a2379f
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-20 14:30:32 -0500

    added right sidebar scroll functionality when navigating baby steps

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,22 @@
-'use strict';
+"use strict";
 
 (function () {
   'use strict';
 
+  var slideRightSidebar = function slideRightSidebar(stepNumber) {
+    $("#baby-steps-panel").css("top", "-" + (stepNumber - 1) * 800 + "px");
+  };
+  slideRightSidebar(1);
+
   $.makeArray($('#left-sidebar').find('li')).map(function (value) {
-    $('#' + value.getAttribute('id')).parent().click(function (event) {
-      var clickedElement = $('#' + event.currentTarget.getAttribute('id'));
+    $("#" + value.getAttribute('id')).parent().click(function (event) {
+      var clickedElement = $("#" + event.currentTarget.getAttribute('id'));
       var clickedIdNumber = parseInt(event.currentTarget.getAttribute('id').split("-")[1]);
-      console.log(clickedIdNumber);
       clickedElement.find('li').addClass('active');
       clickedElement.siblings().find('li').removeClass('active');
-      $('#transition-bar').css('top', 61 * (clickedIdNumber - 1) + 'px');
+      $('#transition-bar').css('top', 61 * (clickedIdNumber - 1) + "px");
+      slideRightSidebar(clickedIdNumber);
+      event.preventDefault(); //this is where i'm currently at
     });
   });
 })();
