c08918070fa727be00f708eb6e6519c0c3c19a8d
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-03-20 01:00:56 -0500

    added bower and bower script to package.json for managing front-end dependencies, integrated js transition function for a clicked baby-step li

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,16 @@
-"use strict";
+'use strict';
 
-// main js
+(function () {
+  'use strict';
+
+  $.makeArray($('#left-sidebar').find('li')).map(function (value) {
+    $('#' + value.getAttribute('id')).parent().click(function (event) {
+      var clickedElement = $('#' + event.currentTarget.getAttribute('id'));
+      var clickedIdNumber = parseInt(event.currentTarget.getAttribute('id').split("-")[1]);
+      console.log(clickedIdNumber);
+      clickedElement.find('li').addClass('active');
+      clickedElement.siblings().find('li').removeClass('active');
+      $('#transition-bar').css('top', 61 * (clickedIdNumber - 1) + 'px');
+    });
+  });
+})();
