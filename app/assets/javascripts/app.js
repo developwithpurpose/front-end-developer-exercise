2b0f0ee7a5fac82d186c8670a3eb8425e7e6b4ee
Jennifer <jsmit032@ucr.edu>
2016-03-26 02:22:38 -0700

    current link styles changes and removes on clicks

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
 	// ACTIVE NAVAGATION
 	//-----------------------------------------------
 
-	$('.step-item').on('click', function(event){
-		event.preventDefault();
-
-		var testing = $(this);
-		console.log(testing);
-
-		// $(this).addClass('is-current')
-		// .closest('step-link');
+	$('.step-item').click(function() {
+    // If this isn't already is-current
+	  if (!$(this).hasClass("is-current")) {
+	    // Remove the class from anything that is is-current
+	    $("li.is-current").removeClass("is-current");
+	    // And make this is-current
+	    $(this).addClass("is-current");
+	  }
 	});
 });
