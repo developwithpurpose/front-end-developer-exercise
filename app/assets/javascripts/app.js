ce747cc6a7238b5cb293e4b86ba94f541bfc59cf
Jennifer <jsmit032@ucr.edu>
2016-03-28 14:15:42 -0700

    working on rendering templates based on what is clicked on the nav bar, so far just rendering step-3 template

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -14,6 +14,32 @@ $(document).ready(function(){
 	    $("li.is-current").removeClass("is-current");
 	    // And make this is-current
 	    $(this).addClass("is-current");
+	    console.log(this);
 	  }
 	});
+
+	$.ajax('../../templates/step-3.html', {
+		data: {location: $('.step-item').data('step')},
+		success: function(response){
+			//console.log(response);
+			$('.page-content-container').html(response);
+		},
+		error: function(){
+			$('.page-content-container')
+			.html('<h3>There was an error in loading this step</h3>')
+		}
+	});
 });
+
+//----------------------
+// FUNCTIONS
+//----------------------
+
+function Step(el) {
+	this.el = el;
+
+	//helper method
+	this.loadStep = function() {
+		$.ajax('../../templates/')
+	}
+}
