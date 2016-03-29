6fd7c2914bfa4119176cb995e497393055a6fc65
Jennifer <jsmit032@ucr.edu>
2016-03-28 18:25:43 -0700

    have a function with a helper method to load a step based on the value recieved when clicking on a step-item. Having trouble figuring how to call a helper method when click action is performed

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -18,7 +18,11 @@ $(document).ready(function(){
 	  }
 	});
 
-	$.ajax('../../templates/step-3.html', {
+	$('.step-item').on('click', Step);
+
+	var newStep = new Step($('.step-item'));
+
+	$.ajax('../../templates/step-3', {
 		data: {location: $('.step-item').data('step')},
 		success: function(response){
 			//console.log(response);
@@ -36,10 +40,30 @@ $(document).ready(function(){
 //----------------------
 
 function Step(el) {
+	var step = $(this).closest('.step-item');
+	var num = step.data('step');
 	this.el = el;
+	console.log(num);
 
 	//helper method
 	this.loadStep = function() {
-		$.ajax('../../templates/')
+		$.ajax('../../templates/step-'+num,{
+			success: function(response){
+				$('.page-content-container').html(response);
+			},
+			error: function(){
+				$('.page-content-container')
+				.html('<h3>There was an error in loading this step</h3>')
+			}
+		});
 	}
+	console.log(this.el);
+	//el.on('click', this.loadStep);
 }
+
+// function Step() {  
+// 	var step = $(this).closest('.step-item');
+// 	var num = step.data('step');
+// 	var details = $("<p>You've selected step # "+(num)+"</p>");
+// 	console.log(details);
+// }
