72ff49bf555c08ffd3577b6129dfb5f875045133
Nick Daniel <ndaniel@imagehawk.com>
2016-04-01 17:11:03 -0500

    added tab animation

--- app/assets/javascripts/.keep.js
+++ app/assets/javascripts/.keep.js
@@ -1,22 +1,36 @@
 
 
 $(document).ready( function() {
+    
+    // Render
     $.ajax({
         type: 'GET',
         url: "../baby-steps.json",
         dataType: 'json',
-        success: function(data) {
-            //console.log('Friends', data);
-            var list = data;
-            console.log(list)            
+    })
+    .success (function(data) {
+        //console.log('Friends', data);
+        var friends = data.friends;
+        
+        var sorted = {};
+        for (friend in friends) {
+            person = friends[friend]
+            //console.log('Friends', person)
+            $('#babystep-' + person["babyStep"]).append("<div><span>" + person["firstName"] + ' ' +  person["lastName"] + "</span></div>");
         }
+        
     });
     
+    
+    // Transition Functions 
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    $('#tabTrans li').click( function() {
+        $(this).addClass('animated slideInDown').one(animationEnd, function() {
+            $(this).removeClass('animated slideInDown')
+        });
+        $('div.active').addClass('animated slideOutUp').one(animationEnd, function() {
+            $('div.active').removeClass('animated slideOutUp');
+        });
+    });
 });
-
-
-
-
-//$.each(data.friends, function (firstName, lastName){
-  //          $('#bs1Friends').append( '<span>' + firstName + '' + lastName + '</span>');
-    //    });
+    
