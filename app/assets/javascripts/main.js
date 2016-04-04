922a338defeef1781142985b30790e570565b70c
CarbonSam <samschu@live.com>
2016-04-04 13:16:08 -0700

    Add sliding animation for baby step content.

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function ($) {
+    
+    $(".baby-step").slideUp();
+    $(".baby-step.active").slideDown();
+
+    // Tabbed Navigation for the Baby Steps
+    $("nav a.tab").click(function () {
+        
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+
+        $(".baby-step").slideUp();
+
+        var content_show = $(this).attr("title");
+        $("#" + content_show).slideDown();
+        $("#" + content_show).addClass("active");
+        
+        return false;
+    });
+
+});
