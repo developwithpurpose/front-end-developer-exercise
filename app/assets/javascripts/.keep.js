c606b63a77a4dcb6bad628dc80046ef01b25060f
Nick Daniel <ndaniel@imagehawk.com>
2016-04-04 17:59:31 -0500

    added bootstrap fade and friends list added to tab-panes

--- app/assets/javascripts/.keep.js
+++ app/assets/javascripts/.keep.js
@@ -11,11 +11,58 @@ $(document).ready( function() {
         //console.log('Friends', data);
         var friends = data.friends;
         
-        var sorted = {};
+        /*var sorted = {};
         for (friend in friends) {
             person = friends[friend]
             //console.log('Friends', person)
             $('#babystep-' + person["babyStep"]).append("<div><span>" + person["firstName"] + ' ' +  person["lastName"] + "</span></div>");
+        }*/
+        
+        var friends = data.friends;
+        var sorted = {};
+        for (friend in friends) {
+            if (sorted[friends[friend].babyStep] == undefined) {
+            sorted[friends[friend].babyStep] = [];
+            }
+            sorted[friends[friend].babyStep].push(friends[friend]);
+            //console.log(sorted);
+        }
+        for (stepIndex in sorted) {
+            var text = '';
+            step = sorted[stepIndex];
+            var babyStep = step[0]['babyStep'];
+            var numPeople = step.length; 
+            if (numPeople <= 2) {   
+                var names = [];
+                for (personIndex in step) {
+                    names.push(step[personIndex]['firstName'] + " " + step[personIndex]['lastName'] );   
+                }
+                text = names.join();
+                if (numPeople == 1) {
+                    text += ' is on this step. ';
+                }
+                else {
+                    text += ' are on this step. ';
+                }
+                //console.log(text, numPeople);
+            }
+            else {
+                var names = [];
+                for (i = 0; i < 2; i++) {
+                    names.push(step[i]['firstName'] + " " + step[i]['lastName'] );   
+                }
+                text = names.join( ', ' );
+                text = text + ", and " + (numPeople - 2);
+                if ((numPeople - 2) == 1) {
+                    
+                    text += ' Other Friend are on this step.';
+                }
+                else {
+                    text += ' Other Friends are on this step.';
+                }
+                console.log(numPeople)
+            }
+           $('#babystep-' + babyStep).append("<div class='col-xs-8'><span>" + text  + "</span></div>");
         }
     });
     
