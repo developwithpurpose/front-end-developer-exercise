42f8983911a1d8d58d71c0857791caf3104a58fb
CarbonSam <samschu@live.com>
2016-04-07 15:15:38 -0700

    Load and display data from JSON file.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -21,4 +21,67 @@ jQuery(document).ready(function ($) {
         return false;
     });
 
+
+    // Display data from the JSON file
+    var displayFriends = function (response) {
+
+        var allFriends = response.friends;
+
+        for (var currentBabyStep = 0; currentBabyStep < allFriends.length; currentBabyStep++) {
+
+            var currentBabyStepFriends = allFriends.filter(function (item) {
+                return item.babyStep === currentBabyStep
+            });
+
+            if (currentBabyStepFriends.length > 0) {
+
+                var totalFriends = currentBabyStepFriends.length;
+                var numFriendsAdded = 0;
+                var maxFriendsToAdd = 2;
+                var messageHTML = "";
+
+                // Add the first two friend names to the list
+                for (var x = 0; x < maxFriendsToAdd; x++) {
+
+                    if (numFriendsAdded === 0) {
+                        messageHTML += "<span class='friend'>" + currentBabyStepFriends[x].firstName + " " + currentBabyStepFriends[x].lastName + "</span>";
+                    } else if (totalFriends === 2 && numFriendsAdded < 2) {
+                        messageHTML += " and <span class='friend'>" + currentBabyStepFriends[x].firstName + " " + currentBabyStepFriends[x].lastName + "</span>";
+                    } else if (totalFriends > 2 && numFriendsAdded < 2) {
+                        messageHTML += ", <span class='friend'>" + currentBabyStepFriends[x].firstName + " " + currentBabyStepFriends[x].lastName + "</span>";
+                    }
+                    numFriendsAdded++;
+
+                }
+
+                // Add number of other friends as well as the baby step number
+                switch (totalFriends) {
+                case 1:
+                    messageHTML += " is also in Baby Step " + currentBabyStep;
+                    break;
+                case 2:
+                    messageHTML += " are also in Baby Step " + currentBabyStep;
+                    break;
+                case 3:
+                    messageHTML += ", and 1 other friend are also in Baby Step " + currentBabyStep;
+                    break;
+                default:
+                    messageHTML += ", and " + (totalFriends - numFriendsAdded) + " other friends are also in Baby Step " + currentBabyStep;
+                }
+
+                // Display list of friends on the page
+                var allFriendsSelector = "#baby_step_" + currentBabyStep + "_content footer p";
+                $(allFriendsSelector).html(messageHTML);
+            }
+
+        }
+
+    };
+
+    // Load JSON data
+    var $friendsList = $.getJSON(
+        "./baby-steps.json",
+        displayFriends
+    );
+
 });
