b92c7cd9913cd99061c045fe61f427f4b80e1f2e
Matthew Morrison <m.james.morrison00@gmail.com>
2016-04-29 22:12:18 -0400

    Finished javascript!
    Pretty much done…just got to ensure IE9 compatibility and smooth out
    that transition.
    
    Also the icons are not of uniform size. ?!?!

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,8 @@
 // Main Javascript File
 
-// Wait for the DOM to lead before firing off our Javascript.
-// This doesn't have a huge impact since this script is nearly
-// the last thing loaded...but, ya know, habbit.
-
+// Load the DOM first, then initialize our scripts
 $(document).ready(function() {
-	console.log("Document is ready!");
+	// console.log("Document is ready!");
 	ramsey.init();
 });
 
@@ -13,19 +10,20 @@ var ramsey = {
 
 	// Initalize allllll dem functions
 	init: function() {
-		console.log("Initialize");
+		// console.log("Initialize");
 		this.listeners();
+		this.getFriendsBabySteps();
 	},
 
 	// This is where we set up our scene listeners
 	listeners: function() {
-		console.log("Listeners");
+		// console.log("Listeners");
 		$('.main-navigation a').on('click', this.navToggle);
 	},
 
 	// Toggle navigation items, obtain content target
 	navToggle: function(e) {
-		console.log("navToggle");
+		// console.log("navToggle");
 		e.preventDefault();
 		var $currentNav = $(e.target),
 			$lastNav,
@@ -44,7 +42,7 @@ var ramsey = {
 
 	// Swap out the content based on the navigation item selected
 	contentToggle: function(element) {
-		console.log("Target " + element.attr("id"));
+		// console.log("Target " + element.attr("id"));
 		var $currentContent = $(element),
 			$lastContent;
 		
@@ -57,5 +55,91 @@ var ramsey = {
 		// setTimeout(function() {
 			$currentContent.addClass('active');
 		// }, 200);
+	},
+
+	// Jquery - AJAX method for getting our friend's baby steps
+	getFriendsBabySteps: function() {
+		console.log("Baby steps!");
+
+		$.ajax({
+			url: './baby-steps.json',
+			success: function(response) {
+				// console.log("Success!");
+				ramsey.organizeFriendsBabySteps(response["friends"]);
+			},
+			error: function(response) {
+				console.log(response.status + " " + response.statusText);
+			}
+		});
+	},
+
+	// Now we sort through our "friends" object and organize them by step
+	organizeFriendsBabySteps: function(friends) {
+		// Iterate through our friend list
+		// If this baby step = next baby step,
+		// Create a "users" object & add them
+		// Otherwise, send off to our display function!
+		var friendList = [];
+		var currentStep = friends[0]["babyStep"];
+
+		for (var i = 0; i < friends.length; i++) {
+
+			if (friends[i]["babyStep"] == currentStep) {
+				friendList.push(friends[i]);
+			} else {
+				
+				// sort by last name ascending
+				this.sortFriendsBabySteps(friendList);
+				this.displayFriendsBabySteps(friendList);
+				friendList = [];
+				friendList.push(friends[i]);
+				currentStep ++;
+			}
+		}
+
+		// sort by last name ascending
+		this.sortFriendsBabySteps(friendList);
+		this.displayFriendsBabySteps(friendList);
+		
+	},
+
+	// Sort the list alphabetically by their last names
+	sortFriendsBabySteps: function(friends) {
+		friends.sort(function(a, b) {
+			var textA = a.lastName.toUpperCase();
+	    	var textB = b.lastName.toUpperCase();
+	    	return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+		});
+	},
+
+	// Displaying our friend's baby steps
+	displayFriendsBabySteps: function(friends) {
+		console.log(friends);
+		var currentStep = friends[0]["babyStep"];
+		var $babyStepDiv = $('.content-component[data-baby-step="' + currentStep + '"] .other-users');
+		var usersText = '';
+
+		usersText = '<p><a href="#">' + friends[0]["firstName"] + ' ' + friends[0]["lastName"] + '</a>';
+
+		if (friends.length < 2) {
+			usersText += ' is also ';
+		}
+
+		else if (friends.length < 3) {
+			usersText += ' and <a href=#$">'
+			+ friends[1]["firstName"] + ' ' + friends[1]["lastName"]+ '</a>'
+			+ ' are also in ';
+		}
+		else if (friends.length >= 3) {
+			usersText += ', <a href="#">'
+			+ friends[1]["firstName"] + ' ' + friends[1]["lastName"]+ '</a>'
+			+ ' and ' + (friends.length - 2) + ' other';
+			friends.length == 3 ? usersText += ' friend ' : usersText += ' friends';
+			usersText += ' are also';
+		}
+
+		usersText += ' in Baby Step ' + currentStep + '</p>';
+
+		$babyStepDiv.html(usersText);
 	}
 }
