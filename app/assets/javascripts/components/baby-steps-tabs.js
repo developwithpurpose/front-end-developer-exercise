1004c1f9b533df54803d62dac3b280fcd19c3c32
amoradi <moradi.aaron@gmail.com>
2016-05-17 15:31:15 -0400

    css animations and transitions jquery fallback

--- app/assets/javascripts/components/baby-steps-tabs.js
+++ app/assets/javascripts/components/baby-steps-tabs.js
@@ -5,6 +5,10 @@ $(function() {
 		$tab1 = $component.find("#step1Tab"),
 		hash = window.location.hash.substring(1);
 		
+		function activateTab1() {
+			$tab1.addClass("active");
+		}
+
 		if (hash) {
 			switch (hash) {
 				case "baby-step-1":
@@ -22,14 +26,42 @@ $(function() {
 			activateTab1();
 		}
 
-		function activateTab1() {
-			$tab1.addClass("active");
-		}
-
 		$tabs.on('click', function() {
+			var $this = $(this),
+				$tabContent = $this.next(".content"),
+				$allTabContent = $component.find(".content").not($tabContent);
+
 			$tabs.each(function() {
-				$(this).removeClass("active");
+				$this.removeClass("active");
 			});
+
+			if ($("html").hasClass("no-cssanimations")) {
+				$this
+				 .animate({
+				 	"padding-left":"80px",
+				 	"padding-right":"64px"
+				 },167)
+				 .animate({
+				 	"padding-left": "130px",
+				 	"padding-right":"14px"        
+				 },167)
+				 .animate({
+				 	"padding-left": "120px",
+				 	"padding-right":"24px"        
+				 },167);
+			}
+
+			if ($("html").hasClass("no-csstransitions")) {
+				$tabContent.animate({
+					"left": "300px",
+					"opacity": "1"
+				}, 300);
+
+				$allTabContent.animate({
+					"left": "200px",
+					"opacity": "0"
+				}, 300);
+			}
 		});
 
 	});
