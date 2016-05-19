561628a81440f9ee76c22d3c8c9aaa598bbe6e88
Aaron Moradi <moradi.aaron@gmail.com>
2016-05-19 01:54:22 -0400

    added lint task
    - lints js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var cssnano = require('gulp-cssnano');
 var rename = require("gulp-rename");
 var concat = require('gulp-concat'); 
 var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var eslint = require('gulp-eslint');
 
 var componentJs = './app/assets/javascripts/components/**/*.js';
 var vendorJs = './app/assets/javascripts/vendor/**/*.js';
@@ -14,19 +16,25 @@ var cssDest = './app/assets/stylesheets/';
 gulp.task('compile-less', function() {
    return gulp.src('./app/assets/stylesheets/less/main.less')
         .pipe(less())
-        .pipe(gulp.dest(cssDest))
+        .pipe(autoprefixer({browsers: ['> 1%'], cascade: false}))
         .pipe(cssnano())
         .pipe(rename("main.min.css"))
         .pipe(gulp.dest(cssDest));
 });
 
+gulp.task('lint', function () {
+    return gulp.src([componentJs,'!node_modules/**']) 
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
 gulp.task('scripts', function() {  
     return gulp.src([vendorJs, componentJs])
         .pipe(concat('main.js'))
-        .pipe(gulp.dest(jsDest))
         .pipe(uglify())
         .pipe(rename("main.min.js"))
         .pipe(gulp.dest(jsDest));
 });
 
-gulp.task('default', ['compile-less', 'scripts']);
+gulp.task('default', ['compile-less', 'scripts', 'lint']);
