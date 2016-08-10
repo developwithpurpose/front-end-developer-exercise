6143d5c078568753d9e042a18de9eb9d2fbbf371
Angelahm <Angela.hm13@gmail.com>
2016-08-10 14:41:53 -0500

    Added JS and JQuery Files
    Added JS and jQuery for the tab menu and page features.

---
+++ app/assets/javascripts/tabs.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {  
+    $( function() {
+    $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+    $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+  } );
+});
+
