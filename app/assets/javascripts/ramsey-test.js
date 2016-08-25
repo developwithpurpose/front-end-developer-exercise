7340124e12eee6e84a67af591b6049e710571791
Raphael Villas <raphael.villas@gmail.com>
2016-08-24 19:56:09 -0500

    Animating main area when clicking on navigation items

--- app/assets/javascripts/ramsey-test.js
+++ app/assets/javascripts/ramsey-test.js
@@ -2,11 +2,23 @@ var resetLinkColors = function () {
   $("nav li").filter(".active").removeClass("active");
 };
 
+function moveToAnchor(id){
+  var anchorLocation = $("a[name='a-"+ id +"']").offset().top;
+  $("main").animate(
+    { scrollTop: anchorLocation },
+    1000
+  );
+}
+
+
 var setUp = function () {
-  // Highlight icons as nav is clicked
-  $("nav li a").on("click", function () {
+  $("nav li a").on("click", function (e) {
+    e.preventDefault();
     resetLinkColors();
     $(this).parent().addClass("active");
+
+    var targetId = $(this).parent().attr('id')
+    moveToAnchor(targetId);
   });
 };
 
