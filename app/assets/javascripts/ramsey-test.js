1405be43f98dfb46be3abc1b128b9e962050294d
Raphael Villas <raphael.villas@gmail.com>
2016-08-29 18:49:25 -0500

    Getting friends alert up and running

--- app/assets/javascripts/ramsey-test.js
+++ app/assets/javascripts/ramsey-test.js
@@ -40,9 +40,68 @@ var setUp = function () {
     var targetId = $(this).parent().attr('id')
     moveToAnchor(targetId);
     animateNav(targetId);
+    displayFriendsInfoFor(targetId);
   });
 };
 
+var processReturnedData = function ( data ) {
+  // Sort into a friends object that stores an array of first names grouped by baby step
+  //
+  // friends {
+  // step1: ["Sharon","Bobby","Arun"]
+  // }
+
+  window.friends = {};
+  var friendsArray = data['friends'];
+
+  for (var i in friendsArray){
+    var stepKey = "step" + (friendsArray[i]['babyStep']);
+    if (friends[stepKey] === undefined) {
+      friends[stepKey] = new Array(friendsArray[i]['firstName'])
+    } else {
+      friends[stepKey].push(friendsArray[i]['firstName'])
+    }
+  }
+};
+
+var fetchData = function () {
+  $.ajax({
+    url: "./baby-steps.json",
+    context: document.body
+  }).done(function( returnedData ) {
+    processReturnedData( returnedData );
+  });
+};
+
+var displayFriendsInfoFor = function (step) {
+  var displayText;
+  if (friends[step] === undefined) {
+    displayText = "";
+  } else {
+    displayText = friends[step];
+  }
+  displayText += displayPredicate(step);
+  $("#friends-alert").text(displayText);
+};
+
+var displayPredicate = function (step) {
+  if(window.friends[step] === undefined) {
+    return "";
+  };
+
+  var tense;
+  switch (window.friends[step] != undefined && window.friends[step].length) {
+    case 1:
+      tense = "is also"
+      break;
+    default:
+      tense = "are also"
+      break;
+  }
+  return " " + tense + " in " + step;
+}
+
 $(function () {
   setUp();
+  fetchData();
 });
