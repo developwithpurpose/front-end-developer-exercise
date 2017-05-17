4976a9d538e01ac9798428b85a73953fd454612f
Paul Furiani <pf@furiani.net>
2017-05-17 11:08:03 -0500

    adding vertical carousel & transition effects

--- resources/assets/js/nav.js
+++ resources/assets/js/nav.js
@@ -1,5 +1,9 @@
+//require('./lib/flux/flux.jquery.min');
+
 $(document).ready(function() {
 	$('.nav-main .nav-item a').click(function(event) {
+		$(this).parent().siblings().find('.active').removeClass('active');
+		$(this).addClass('active');
 		// TODO: Change Step
 		var step = $(this).data('step');
 		console.log('nav item clicked: Step ' + step);
