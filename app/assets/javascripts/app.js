01246aeea10eb90da503d9487b8cdda79fb33482
Mark Fasel <me@markfasel.com>
2017-05-21 10:18:56 -0400

    Css3 transition fallback to jQuery animate
    On content block transition - fallback using modernizr and jQuery
    animate

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -17,17 +17,41 @@ $(document).ready(function() {
 			// Prevent page jump
 			event.preventDefault();
 			
-			// Remove Classes from everything before we reapply
-			$(".content--main section.active").addClass("fade-out-up").removeClass("active"); // Remove from section
-			//$allStepItems.removeClass("active"); // Remove from nav item
+			// Let's check that the item clicked isn't the current item - if so return false to prevent a double click
+			if($(this).parent().hasClass('active')){
+				return false;
+		   	}
 			
-			var $currentItem = $('nav li.active');
-			$currentItem.addClass("fade-out-up").removeClass('active');
+			// Let's check modernizr to see if the browser supports css transitions. 
+			// If not, let's animate with JS, if it does we'll animate using CSS3
+			if(!Modernizr.csstransitions) { 
+				$(".content--main section.active").animate({top: '-500px', opacity: 0}, {duration: 700, easing: 'easeInQuart'}).removeClass("active");
+				$(".content--main section#step"+$(this).data('step')).delay(700).animate({top: '0' , opacity: 1}, {duration: 700, easing: 'easeOutQuart'}).addClass("active");
+			} else {
+				$(".content--main section.active").addClass("fade-out-up").removeClass("active"); // Remove from section
+				$(".content--main section#step"+$(this).data('step')).removeClass('hidden').addClass("active");
+			}			
+			
+			var $currentItem = $('nav li.active'); // Sets Current Nav Item
+			$allStepItems.removeClass('active'); // Remove 
 			
 			// Add active class to the newly selected item
 			$(this).parent().addClass("active active--step" + $(this).data('step'));
-			$(".content--main section#step"+$(this).data('step')).removeClass('hidden').addClass("active");
+			
+			
 		});
+		
+		$slideOutAnimationFallback = function() {
+			if(!Modernizr.csstransitions) { // Test if CSS transitions are supported
+	            $(function() {
+	                $('#js').hover(function(){
+	                    $(this).animate({width:'50px',height:'50px'},{queue:false,duration:500});
+	                }, function(){
+	                    $(this).animate({width:'100px',height:'100px'},{queue:false,duration:500});
+	                });
+	            });
+	        }
+		}
 
     };
 
