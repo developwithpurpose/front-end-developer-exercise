53424e2c9cd294fdf0f14b2c415dcf29f43f60fb
Matthew Pritchett <matthew.r.pritchett@gmail.com>
2017-05-26 10:58:24 -0400

    Adding JS scripts.

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,15 @@
+(function($) {
+
+	$('#contentSlider').magicSlider({
+		autoHeight: false,
+		dynamicArrows: false,
+		dynamicTabs: false,
+		externalTriggerSelector: "a.content__list-item",
+		highlightExternalTrigger: true,
+		panelTitleSelector: "",
+		slideEaseDuration: 500,
+		slideDirection: "vertical",
+		carousel: false,
+	});
+
+})( jQuery );
