6029fe4b5ef94e0b4458826480c446367345a746
Michael Tucker <mstucker@users.noreply.github.com>
2017-06-16 16:05:21 -0500

    Add files via upload

---
+++ app/README.markdown
@@ -0,0 +1,18 @@
+Document your submission here (optional, but recommended)
+
+So this is my second submission after receiving an email last week from Josh Sikes.  
+
+As far as the project goes:
+
+I picked up from where I left off on the last submission.  Even though there were changes to the 
+instructions, I felt that most of the work I had done before still applied.  The main things I focused
+on were cleaning up things that I could see.  I also attempted to create a CSS file with CSS3 transitions
+and transformations to meet the criteria for having the app work without js.  I was not as successful 
+in doing this as I would've liked.  I resorted to just showing everything and keeping the menu in a 
+fixed location (which looks horrible).  But I valued the function over the look.  
+
+Overall, I feel okay about the submission.  I believe the core is good, and I could've focused on so
+other ways to improve the work from my previous solution, but I chose what I chose, and ended up doing
+what I could to the project as best as I could at this time. I definitely enjoyed getting back into
+a heavier project to remind me of where I am now, and where I'd like to be. Thank you for the opportunity. 
+
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,5 @@
 function grabJSON (){  //This function grabs the information from the JSON file and determines how many people are on each baby step and displays the appropriate information on the webpage.
-	$.getJSON("assets/javascripts/baby-steps.json", function(response){
+	$.getJSON("/../../baby-steps.json", function(response){
 			var $onBabyStepString = $("li.active").find("a").attr("href").slice(-1);
 			var $onBabyStepInt = parseInt($onBabyStepString, 10);
 			var names= [];
@@ -11,23 +11,23 @@ function grabJSON (){  //This function grabs the information from the JSON file 
 			if (names.length === 0) {
 				$(".replace-this").replaceWith("<p class='replace-this'></p>");
 			} else if (names.length === 1) {
-				$(".replace-this").replaceWith("<p class='replace-this'><a>" + names[0].firstName +" "+ names[0].lastName + "</a> is also on Baby Step " +$onBabyStepInt+".</p>");
+				$(".replace-this").replaceWith("<p class='replace-this'><a>" + names[0].firstName +" "+ names[0].lastName + "</a> is also in Baby Step " +$onBabyStepInt+".</p>");
 			} else if (names.length === 2) {
-				$(".replace-this").replaceWith("<p class='replace-this'><a>" + names[0].firstName +" "+ names[0].lastName + "</a> and <a> " + names[1].firstName +" "+ names[1].lastName + "</a> are also on Baby Step " +$onBabyStepInt+".</p>");
+				$(".replace-this").replaceWith("<p class='replace-this'><a>" + names[0].firstName +" "+ names[0].lastName + "</a> and <a> " + names[1].firstName +" "+ names[1].lastName + "</a> are also in Baby Step " +$onBabyStepInt+".</p>");
 			} else if (names.length === 3) {
-				$(".replace-this").replaceWith("<p class='replace-this'><a>" + names[0].firstName +" "+ names[0].lastName + "</a>, <a>" + names[1].firstName +" "+ names[1].lastName +"</a> and 1 other friend are also on Baby Step "+$onBabyStepInt+".</p>");
+				$(".replace-this").replaceWith("<p class='replace-this'><a>" + names[0].firstName +" "+ names[0].lastName + "</a>, <a>" + names[1].firstName +" "+ names[1].lastName +"</a> and 1 other friend are also in Baby Step "+$onBabyStepInt+".</p>");
 			} else {
-				$(".replace-this").replaceWith("<p class='replace-this'><a>" + names[0].firstName +" "+ names[0].lastName + "</a>, <a>" + names[1].firstName +" "+ names[1].lastName +"</a> and "+(names.length - 2)+" other friends are also on Baby Step "+$onBabyStepInt+".</p>");
+				$(".replace-this").replaceWith("<p class='replace-this'><a>" + names[0].firstName +" "+ names[0].lastName + "</a>, <a>" + names[1].firstName +" "+ names[1].lastName +"</a> and "+(names.length - 2)+" other friends are also in Baby Step "+$onBabyStepInt+".</p>");
 			}
 	});
 }
 
 $(function() {
 	grabJSON();//This is where the page loads.  I wanted to call the function first incase the friends at baby step one ever changed.
-
+	
 	var height = $(".wrapper").height();
-  $("#shadow-left").height(height);
-  $("#shadow-right").height(height);
+  	$("#shadow-left").height(height);
+  	$("#shadow-right").height(height);
 
 	$(".baby-steps-list").each(function(){ //This gives names to all the different elements I will use.  This was done with the help of "JavaScript and jQuery: interactive front-end web development" by Jon Duckett
 		var $this  = $(this);
@@ -64,4 +64,3 @@ $(function() {
     return window.innerHeight - $(this).offset().top - 35;
 	});
 });
-
