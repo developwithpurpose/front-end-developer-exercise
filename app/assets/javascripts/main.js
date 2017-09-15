6b0dd2246b8e0dba072267b36bc94b8f1139638e
Josh Wilkerson <jgwilkerson@gmail.com>
2017-09-15 09:25:08 -0500

    added dynamic AJAX "friends" data

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -29,3 +29,70 @@ navLinks.forEach(function (link) {
     });
   });
 });
+
+// create empty arrays to hold "friends" data
+var friendsArray = [];
+for (var i = 0; i < steps.length; i++) {
+  friendsArray[i] = [];
+}
+
+// get ajax data
+function getJSONdata(path, callback) {
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      var data = JSON.parse(request.responseText);
+      if (callback) callback(data);
+    }
+  };
+  request.open('GET', path);
+  request.send();
+}
+
+var friends = void 0;
+var showFriends = document.querySelectorAll('.friends');
+getJSONdata('../baby-steps.json', function (data) {
+  friends = data.friends;
+
+  // sort data by last name
+  friends = friends.sort(function (a, b) {
+    var a1 = a.lastName;
+    var b1 = b.lastName;
+    return a1 < b1 ? -1 : a1 > b1 ? 1 : 0;
+  });
+
+  // for each friend, push to cooresponding "friendsArray" based on babyStep value
+  for (var i = 0; i < friends.length; i++) {
+    var friend = friends[i];
+
+    for (var j = 0; j < steps.length; j++) {
+      if (friend.babyStep == j + 1) {
+        friendsArray[j].push(friend);
+      }
+    }
+  }
+
+  // insert friends data
+  showFriends.forEach(function (friendElement) {
+    var parentIndex = parseInt(friendElement.parentElement.id.substring(8) - 1);
+    var sameStepFriends = friendsArray[parentIndex];
+
+    // if number of friends is greater than 0
+    if (sameStepFriends.length > 0) {
+      // if one friend
+      if (sameStepFriends.length == 1) {
+        friendElement.innerHTML = '\n          <a href="#_">' + sameStepFriends[0].firstName + ' ' + sameStepFriends[0].lastName + '</a> is also in Baby Step ' + (parentIndex + 1) + '\n        ';
+        // if 2 friends
+      } else if (sameStepFriends.length == 2) {
+        friendElement.innerHTML = '\n          <a href="#_">' + sameStepFriends[0].firstName + ' ' + sameStepFriends[0].lastName + '</a> and\n          <a href="#_">' + sameStepFriends[1].firstName + ' ' + sameStepFriends[1].lastName + '</a>\n          are also in Baby Step ' + (parentIndex + 1) + '\n        ';
+        // if 3 friends
+      } else if (sameStepFriends.length == 3) {
+        friendElement.innerHTML = '\n          <a href="#_">' + sameStepFriends[0].firstName + ' ' + sameStepFriends[0].lastName + '</a>,\n          <a href="#_">' + sameStepFriends[1].firstName + ' ' + sameStepFriends[1].lastName + '</a> and one other friend are also in Baby Step ' + (parentIndex + 1) + '\n        ';
+        // if more than 3 friends
+      } else if (sameStepFriends.length > 3) {
+        friendElement.innerHTML = '\n          <a href="#_">' + sameStepFriends[0].firstName + ' ' + sameStepFriends[0].lastName + '</a>,\n          <a href="#_">' + sameStepFriends[1].firstName + ' ' + sameStepFriends[1].lastName + '</a> and ' + (sameStepFriends.length - 2) + ' other friends are also in Baby Step ' + (parentIndex + 1) + '\n        ';
+      }
+    }
+  });
+});
--- build/js/main.js
+++ build/js/main.js
@@ -22,3 +22,86 @@ navLinks.forEach(link => link.addEventListener('click', function(e){
     }
   })
 }));
+
+
+
+
+// create empty arrays to hold "friends" data
+let friendsArray = [];
+for (var i = 0; i < steps.length; i++) {
+    friendsArray[i] = [];
+}
+
+// get ajax data
+function getJSONdata(path, callback) {
+  let request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+    if(request.readyState === 4 && request.status === 200) {
+      var data = JSON.parse(request.responseText);
+        if(callback) callback(data);
+      }
+    };
+    request.open('GET', path);
+    request.send();
+  }
+
+
+let friends;
+let showFriends = document.querySelectorAll('.friends');
+getJSONdata('../baby-steps.json', function(data){
+  friends = data.friends;
+
+  // sort data by last name
+  friends = friends.sort((a,b)=> {
+    let a1 = a.lastName;
+    let b1 = b.lastName;
+    return a1<b1 ?-1:a1> b1? 1 :0;
+  })
+
+  // for each friend, push to cooresponding "friendsArray" based on babyStep value
+  for(var i = 0; i < friends.length; i++) {
+    var friend = friends[i];
+
+    for (var j = 0; j < steps.length; j++) {
+      if(friend.babyStep == j + 1) {
+        friendsArray[j].push(friend);
+      }
+    }
+  }
+
+  // insert friends data
+  showFriends.forEach(friendElement => {
+    let parentIndex = parseInt(friendElement.parentElement.id.substring(8) - 1);
+    let sameStepFriends = friendsArray[parentIndex];
+
+    // if number of friends is greater than 0
+    if(sameStepFriends.length > 0 ){
+      // if one friend
+      if(sameStepFriends.length == 1) {
+        friendElement.innerHTML = `
+          <a href="#_">${sameStepFriends[0].firstName} ${sameStepFriends[0].lastName}</a> is also in Baby Step ${parentIndex + 1}
+        `;
+      // if 2 friends
+      } else if(sameStepFriends.length == 2) {
+        friendElement.innerHTML = `
+          <a href="#_">${sameStepFriends[0].firstName} ${sameStepFriends[0].lastName}</a> and
+          <a href="#_">${sameStepFriends[1].firstName} ${sameStepFriends[1].lastName}</a>
+          are also in Baby Step ${parentIndex + 1}
+        `;
+      // if 3 friends
+      } else if(sameStepFriends.length == 3) {
+        friendElement.innerHTML = `
+          <a href="#_">${sameStepFriends[0].firstName} ${sameStepFriends[0].lastName}</a>,
+          <a href="#_">${sameStepFriends[1].firstName} ${sameStepFriends[1].lastName}</a> and one other friend are also in Baby Step ${parentIndex + 1}
+        `;
+      // if more than 3 friends
+      } else if(sameStepFriends.length > 3) {
+        friendElement.innerHTML = `
+          <a href="#_">${sameStepFriends[0].firstName} ${sameStepFriends[0].lastName}</a>,
+          <a href="#_">${sameStepFriends[1].firstName} ${sameStepFriends[1].lastName}</a> and ${sameStepFriends.length - 2} other friends are also in Baby Step ${parentIndex + 1}
+        `;
+      }
+    }
+  });
+});
