7b2745555a4bee946bd7145362490eb9cb07cd90
Jacob Allen <jedijake@me.com>
2017-09-17 22:06:09 -0700

    create function that loops through dom elements and assigns ID's for later use

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,21 @@
-(function($){
+// (function($){
   console.log('main.js loaded');
-  console.log($);
-})(jQuery)
+  var babyStepsList    = document.querySelector('.baby-steps__list').children;
+  var babyStepsContent = document.querySelector('.baby-steps__content').children;
+  function loopThroughBabyStepsAndAssignIds() {
+    for (i=0;i<babyStepsList.length;i++) {
+      console.log(i);
+      var childListItem  = babyStepsList;
+      var childParagraph = babyStepsContent;
+      assignIdToBabyStepsListItem(childListItem[i], i);
+      assignIdToBabyStepsParagraph(childParagraph[i], i);
+    }
+  }
+  function assignIdToBabyStepsListItem(li, idx) {
+    console.log(li);
+    li.id = 'baby-step-list-' + idx;
+  }
+  function assignIdToBabyStepsParagraph(p, idx) {
+    p.id = 'baby-step-p-' + idx;
+  }
+// })(jQuery)
