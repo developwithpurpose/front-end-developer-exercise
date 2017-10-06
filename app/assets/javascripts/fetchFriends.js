399e42a9c63feab9988b21c6679da252b0224716
Josh White <whitejosh42@gmail.com>
2017-10-06 15:29:13 -0500

    forgot to transpile ES6 to IE speak...  added transpiled code as well as original

--- app/assets/javascripts/fetchFriends.js
+++ app/assets/javascripts/fetchFriends.js
@@ -1,70 +1,58 @@
-const sortFriendsByStep = friendList => {
-  const friendsByStep = {}
-  friendList.forEach(friend => {
+'use strict';
+
+var sortFriendsByStep = function sortFriendsByStep(friendList) {
+  var friendsByStep = {};
+  friendList.forEach(function (friend) {
     if (!friendsByStep.hasOwnProperty(friend.babyStep)) {
-      friendsByStep[friend.babyStep] = []
+      friendsByStep[friend.babyStep] = [];
     }
-    friendsByStep[friend.babyStep].push(friend)
-  })
-  return friendsByStep
-}
+    friendsByStep[friend.babyStep].push(friend);
+  });
+  return friendsByStep;
+};
 
-
-const createSupportMessage = (friends, step) => {
-  const numFriends = friends.length
-  const messageTail = `also in Baby Step ${step}`
-  const stepHash = `#step${step}`
-  let message
+var createSupportMessage = function createSupportMessage(friends, step) {
+  var numFriends = friends.length;
+  var messageTail = 'also in Baby Step ' + step;
+  var stepHash = '#step' + step;
+  var message = void 0;
 
   if (numFriends === 1) {
-    message = `
-    <a href="${stepHash}">${friends[0].firstName} ${friends[0].lastName}</a>
-    is ${messageTail}`
-  }
-  else if (numFriends === 2) {
-    message = `
-    <a href="${stepHash}">${friends[0].firstName} ${friends[0].lastName}</a> and
-    <a href="${stepHash}">${friends[1].firstName} ${friends[1].lastName}</a>
-    are ${messageTail}`
-  }
-  else if (numFriends === 3) {
-    message = `
-    <a href="${stepHash}">${friends[0].firstName} ${friends[0].lastName}</a>,
-    <a href="${stepHash}">${friends[1].firstName} ${friends[1].lastName}</a>,
-    and 1 other friend are ${messageTail}`
-  }
-  else if (numFriends > 3) {
-    let numUnseen = numFriends - 2
-    message = `
-    <a href="${stepHash}">${friends[0].firstName} ${friends[0].lastName}</a>,
-    <a href="${stepHash}">${friends[1].firstName} ${friends[1].lastName}</a>,
-    and ${numUnseen} other friends are ${messageTail}`
+    message = '\n    <a href="' + stepHash + '">' + friends[0].firstName + ' ' + friends[0].lastName + '</a>\n    is ' + messageTail;
+  } else if (numFriends === 2) {
+    message = '\n    <a href="' + stepHash + '">' + friends[0].firstName + ' ' + friends[0].lastName + '</a> and\n    <a href="' + stepHash + '">' + friends[1].firstName + ' ' + friends[1].lastName + '</a>\n    are ' + messageTail;
+  } else if (numFriends === 3) {
+    message = '\n    <a href="' + stepHash + '">' + friends[0].firstName + ' ' + friends[0].lastName + '</a>,\n    <a href="' + stepHash + '">' + friends[1].firstName + ' ' + friends[1].lastName + '</a>,\n    and 1 other friend are ' + messageTail;
+  } else if (numFriends > 3) {
+    var numUnseen = numFriends - 2;
+    message = '\n    <a href="' + stepHash + '">' + friends[0].firstName + ' ' + friends[0].lastName + '</a>,\n    <a href="' + stepHash + '">' + friends[1].firstName + ' ' + friends[1].lastName + '</a>,\n    and ' + numUnseen + ' other friends are ' + messageTail;
   }
 
-  return message
-}
+  return message;
+};
 
+var stepSections = [].slice.call(document.getElementsByClassName('step-friends'));
 
-const stepSections = [].slice.call(document.getElementsByClassName('step-friends'))
+var request = new XMLHttpRequest();
+request.responseType = 'json';
 
-const request = new XMLHttpRequest()
-request.responseType = 'json'
-
-request.onreadystatechange = () => {
+request.onreadystatechange = function () {
   if (request.status === 200 && request.response !== null) {
-    const friendsList = request.response.friends
-    const friendsByStep = sortFriendsByStep(friendsList)
+    var friendsList = request.response.friends;
+    var friendsByStep = sortFriendsByStep(friendsList);
 
-    stepSections.forEach((step, idx) => {
-      let day = idx + 1
+    stepSections.forEach(function (step, idx) {
+      var day = idx + 1;
       if (friendsByStep[day]) {
-        const sortedFriends = friendsByStep[day].sort((a, b) => a.lastName > b.lastName)
-        const message = createSupportMessage(sortedFriends, day)
-        step.innerHTML = message
+        var sortedFriends = friendsByStep[day].sort(function (a, b) {
+          return a.lastName > b.lastName;
+        });
+        var message = createSupportMessage(sortedFriends, day);
+        step.innerHTML = message;
       }
-    })
+    });
   }
-}
+};
 
-request.open('GET', './baby-steps.json')
-request.send()
+request.open('GET', './baby-steps.json');
+request.send();
