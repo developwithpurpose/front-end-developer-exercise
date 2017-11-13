88676c946ba0b060ebea1ad8ad64071e855f9a6d
beelarr <bryonl@me.com>
2017-11-13 12:00:05 -0600

    JS built but forEach on line 64 is broken

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,12 +2,15 @@
 
 console.log('its hooked');
 
+var defalutBabyStep = '1';
+var friends;
+
 
 $.ajax({
     url: '../../baby-steps.json',
     success: (data) => {
-         let friends = data.friends;
-        console.log('friends', friends);
+         var friends = data.friends;
+        console.log('friends from ajax', friends);
         displayFriends(friends)
     }
 });
@@ -15,32 +18,35 @@ $.ajax({
 
 
 
-const makeActive =  function () {
-    let clearNav = $('img', this)[0];
-    let fillNav = $('img', this)[1];
-    let currentTab = $(this);
-    let babyStep = $('a', this).attr('datatype');
-    let stepText = $('p', this);
-    let currentActiveClearIcon = $('img', '.active')[0];
-    let currentActiveFillIcon = $('img', '.active')[1]
+var makeActive = function () {
+    var clearNav = $('img', this)[0];
+    var fillNav = $('img', this)[1];
+    var currentTab = $(this);
+    var babyStep = $('a', this).attr('datatype');
+    var stepText = $('p', this);
+    var currentActiveClearIcon = $('img', '.active')[0];
+    var currentActiveFillIcon = $('img', '.active')[1];
 
 
 
     $(currentActiveClearIcon).removeClass('hidden');
     $(currentActiveFillIcon).addClass('hidden');
     $('.active').addClass('inactive').removeClass('active');
-    currentTab.addClass('active');
-    stepText.addClass('active');
-    currentTab.attr('aria-selected', 'true');
-    clearNav.addClass('hidden');
-    fillNav.removeClass('hidden');
+    $('#'+ defalutBabyStep).addClass('hidden');
+
+    $(currentTab).addClass('active');
+    $(stepText).addClass('active');
+    $(currentTab).attr('aria-selected', 'true');
+    $('#'+ babyStep).removeClass('hidden');
+    $(clearNav).addClass('active');
+    $(fillNav).removeClass('hidden');
 
     displayFriends();
 
 
+    console.log('clearIcon', clearNav);
     console.log('this', currentTab);
     console.log('fillIcon', fillNav);
-    console.log('clearIcon', clearNav);
     console.log('babyStep', babyStep);
     console.log('stepText', stepText);
     console.log('CACI', currentActiveClearIcon);
@@ -49,16 +55,44 @@ const makeActive =  function () {
 };
 
 
+
 $(".baby-step-nav-li").click(makeActive);
 
 
 var displayFriends = function (friends) {
-   let stepFriends = [];
-   friends.forEach((friend) => {
-       if (friend.babyStep.to)
-       console.log('friend', friend);
-   })
+   var stepFriends = [];
+   friends.forEach(function(friend) {
+       if(friend.babyStep.toString() === defalutBabyStep){
+           stepFriends.push(friend)
+       }
+        // console.log('friends', friends);
+        // console.log('friend', friend);
+        // console.log('friendBabyStep', friend.babyStep);
+        // console.log('stepFriends', stepFriends);
+   });
 
-}
+    var babyStepText;
+
+    switch(stepFriends.length){
+        case 0:
+            babyStepText = '';
+            break;
+        case 1:
+            babyStepText = `<p class='dynamic-text' <a href="#" class="friend-name">${stepFriends[0].firstName} ${stepFriends[0].lastName}</a> is also on Baby Step ${babyStep} </p>`;
+            break;
+        case 2:
+            babyStepText = `<p class='dynamic-text' <a href="#" class="friend-name">${stepFriends[0].firstName} ${stepFriends[0].lastName} and ${stepFriends[1].firstName} ${stepFriends[1].lastName}</a> are also on Baby Step ${babyStep}</p>`;
+            break
+        case 3:
+            babyStepText = `<p class='dynamic-text' <a href="#" class="friend-name">${stepFriends[0].firstName} ${stepFriends[0].lastName}, ${stepFriends[1].firstName} ${stepFriends[1].lastName}, </a> and ${stepFriends.length -2} other friend is also on Baby Step ${babyStep}</p>`;
+
+        case 4:
+            babyStepText = `<p class='dynamic-text' <a href="#" class="friend-name">${stepFriends[0].firstName} ${stepFriends[0].lastName}, ${stepFriends[1].firstName} ${stepFriends[1].lastName}, </a> and ${stepFriends.length -2} other friends are also on Baby Step ${babyStep}</p>`;
+
+    }
+
+};
+
+
 
 
