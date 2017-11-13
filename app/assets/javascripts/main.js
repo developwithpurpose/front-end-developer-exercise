aee0b23603fd5a2369932c56ef80be6d25ea9f3e
beelarr <bryonl@me.com>
2017-11-13 11:02:24 -0600

    Getting friends from ajax to displayFriends function and added a capture of nav on click

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,64 @@
+"use strict";
+
 console.log('its hooked');
 
 
+$.ajax({
+    url: '../../baby-steps.json',
+    success: (data) => {
+         let friends = data.friends;
+        console.log('friends', friends);
+        displayFriends(friends)
+    }
+});
+
+
+
+
+const makeActive =  function () {
+    let clearNav = $('img', this)[0];
+    let fillNav = $('img', this)[1];
+    let currentTab = $(this);
+    let babyStep = $('a', this).attr('datatype');
+    let stepText = $('p', this);
+    let currentActiveClearIcon = $('img', '.active')[0];
+    let currentActiveFillIcon = $('img', '.active')[1]
+
+
+
+    $(currentActiveClearIcon).removeClass('hidden');
+    $(currentActiveFillIcon).addClass('hidden');
+    $('.active').addClass('inactive').removeClass('active');
+    currentTab.addClass('active');
+    stepText.addClass('active');
+    currentTab.attr('aria-selected', 'true');
+    clearNav.addClass('hidden');
+    fillNav.removeClass('hidden');
+
+    displayFriends();
+
+
+    console.log('this', currentTab);
+    console.log('fillIcon', fillNav);
+    console.log('clearIcon', clearNav);
+    console.log('babyStep', babyStep);
+    console.log('stepText', stepText);
+    console.log('CACI', currentActiveClearIcon);
+    console.log('CAFI', currentActiveFillIcon);
+
+};
+
+
+$(".baby-step-nav-li").click(makeActive);
+
+
+var displayFriends = function (friends) {
+   let stepFriends = [];
+   friends.forEach((friend) => {
+       if (friend.babyStep.to)
+       console.log('friend', friend);
+   })
+
+}
 
 
