e0ae2c4f127c225afe7f96d89abc26bf4ec8390a
Chris McNeese <chris@85supply.com>
2017-12-17 01:08:32 -0600

    update javascript loops

--- app/main.js
+++ app/main.js
@@ -1,9 +1,14 @@
-function show(id) {
-  var thing;
-  thing = document.getElementsByClassName("right");
-  var i;
-  for (i = 0; i < thing.length; i++) {
-    thing[i].style.display = "none";
+function show(id, color) {
+  var content = document.getElementsByClassName("right");
+  var option = document.getElementsByClassName("options");
+  var o;
+  for (o = 0; o < option.length; o++) {
+    option[o].style.background = "blue";
+  }
+  var currentSide = (document.getElementById(color).style.background = "black");
+  var c;
+  for (c = 0; c < content.length; c++) {
+    content[c].style.display = "none";
   }
   var element = (document.getElementById(id).style.display = "block");
 }
