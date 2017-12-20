dedd06d5bfea3bff78f105e9189b6a60230f514d
Chris McNeese <chris@85supply.com>
2017-12-19 18:49:45 -0600

    clean up code and final README notes

--- app/friends.js
+++ app/friends.js
@@ -1,23 +1,9 @@
-// (function() {
-//   var JSONSource = "app/baby-steps.json";
-//   $.getJSON( JSONSource, {
-//     format: "json"
-//   })
-//     .done(function( data ) {
-//         console.log(data.items),
-//       $.each( data.firstName, function( firstName, lastName, babyStep ) {
-//         $( .friends ).appendTo( #steps );
-//         // if ( i === 3 ) {
-//         //   return false;
-//         // }
-//       });
-//     });
 
 var resultsContainer = $("#results");
-$.each(data.friends, function(index, value) { //1. Fetch some data from the server.
-    resultsContainer.append(                  //2. Format all of the names and step information.
-    "<p>step: " +                             //2a. Loop over each friend from my fetched data
-      index +                                 //2b. Append a new paragraph to the results container
+$.each(data.friends, function(index, value) {
+    resultsContainer.append(                  
+    "<p>step: " +                             
+      index + 
       ", firstName: " +
       value.firstName +
       ", lastName: " +
