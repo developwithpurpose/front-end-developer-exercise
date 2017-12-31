1416ffde2d866554d9931fbf4fc3ff098fbb52d0
Mitchell Irvin <mitchell.k.irvin@gmail.com>
2017-12-30 20:09:47 -0500

    fleshed out content for each baby step, added AJAX call and appended content dynamically. AJAX call should work properly for any payload of the same format. TODO: menu transition

--- app/assets/javascripts/babysteps.js
+++ app/assets/javascripts/babysteps.js
@@ -1,6 +1,5 @@
 jQuery( document ).ready(function() {
 
-
   // function to allow for changing of styles in sidenav
   $('.babySteps div a').on('click', function(e) {
     // remove class from previously selected menu item
@@ -10,6 +9,30 @@ jQuery( document ).ready(function() {
 
     // hide initial welcome page
     $('.welcomePage').addClass('displayNone');
+  });
+
+  // ajax call to retrieve friends on each baby step
+  $.get( "../../baby-steps.json", function( data ) {
+    var friendsInEachStep = { '1': [], '2': [], '3': [], '4': [], '5': [], '6': [], '7': [] };
+
+    for(var i = 0; i < data.friends.length; i++) {
+      friendsInEachStep[data.friends[i].babyStep].push(data.friends[i]);
+    }
+
+    for(var i = 1; i < 8; i++) {
+      if(friendsInEachStep[i].length == 0) {
+        // no friends in this step
+      } else if(friendsInEachStep[i].length == 1) {
+        $('#bs' + i + 'Friends').append('<p><a>' + friendsInEachStep[i][0].firstName + ' ' + friendsInEachStep[i][0].lastName + '</a> is also in Baby Step ' + i);
+      } else if(friendsInEachStep[i].length == 2) {
+        $('#bs' + i + 'Friends').append('<p><a>' + friendsInEachStep[i][0].firstName + ' ' + friendsInEachStep[i][0].lastName + '</a> and <a>' +
+          friendsInEachStep[i][1].firstName + ' ' + friendsInEachStep[i][1].lastName + '</a> are also in Baby Step' + i);
+      } else {
+        $('#bs' + i + 'Friends').append('<p><a>' + friendsInEachStep[i][0].firstName + ' ' + friendsInEachStep[i][0].lastName + '</a>, <a>' +
+          friendsInEachStep[i][1].firstName + ' ' + friendsInEachStep[i][1].lastName + '</a> and ' + (friendsInEachStep[i].length - 2) + ' other friend(s) are also in Baby Step ' + i);
+      }
+    }
+
   });
 
 });
