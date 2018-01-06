afbbd7aa4ab7246c897b0af852bb7a10c08d8453
Joe Dodd <joehdodd@gmail.com>
2018-01-06 10:22:51 -0500

    show and hide copy logic--WIP

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,13 +1,42 @@
 const stepNav = document.getElementById('stepNav');
 const navItem = stepNav.getElementsByTagName('li');
+const navItemArr = Array.from(navItem);
 
-for (let i = 0; i < navItem.length; ++i) {
-  navItem[i].addEventListener('click', (e) => {
-    e.preventDefault();
-    let currentActive = document.querySelector('.active');
-    if (currentActive != null) {
-      currentActive.classList.remove('active');
-    }
-    navItem[i].classList.add('active');
-  });
+const stepCopy = document.getElementById('stepCopy');
+const copyItem = stepCopy.getElementsByTagName('div');
+const copyItemArr = Array.from(copyItem);
+
+getActiveItem = (navItemArr, copyItemArr) => {
+  return navItemArr.map( (item, i) => {
+    item.addEventListener('click', (e) =>{
+      e.preventDefault();
+      let currentActiveNav = document.querySelector('.active');
+      if (currentActiveNav !== null) currentActiveNav.classList.remove('active');
+      item.classList.add('active');
+
+      showCopy = (navIndex) => {
+        copyItemArr.map((item, copyIndex) => {
+          console.log(item, copyIndex, navIndex);
+          let currentActiveCopy = document.querySelector('.show-copy');
+          if (currentActiveCopy !== null) {
+            currentActiveCopy.classList.remove('show-copy');
+          }
+          if (copyIndex === navIndex) {
+            console.log(item.classList);
+            item.classList.add('show-copy');
+          }
+        })
+      }
+      showCopy(i);
+    });
+  })
 }
+getActiveItem(navItemArr, copyItemArr);
+
+
+// getCorrectCopy = (navItemArr) => {
+//   return navItemArr.map(item => {
+//     console.log(item);
+//   })
+// }
+// getCorrectCopy(navItemArr);
