e86182bec9a33ed916b1141a453a6420e52a147f
Chase Steely <chasesteely@gmail.com>
2018-03-03 16:21:47 -0600

    active class, scroll, ajax call and sort working

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,29 +1,54 @@
-$(document).ready(function(){
-    $('a[href^="#"]').on('click',function (e) {
-        if ($("a").removeClass("active")) {
-            $(this).removeClass("active");
-        }
-        $(this).addClass("active");
-            // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('main').animate({
-          scrollTop: $(hash).offset().top
-        }, 500, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
+$(document).ready(function () {
+  let friendArray = []
+
+  function reset() {
+    $('a>img:nth-child(even)').addClass("hidden");
+    $('a>img:nth-child(odd)').removeClass("hidden");
+  }
+
+  function nameSort(x, y) {
+    return ((x.lastName == y.lastName) ? 0 : ((x.lastName > y.lastName) ? 1 : -1));
+  }
+  $('a[href^="#"]').on('click', function (e) {
+    $.ajax({
+      url: "../../baby-steps.json",
+      type: 'get',
+      dataType: 'json',
+      success: function (data) {
+        $.each(data.friends, function (index, item) {
+          // console.log(item)
+          friendArray.push(item)
+          // console.log(friendArray)
         });
-      } // End if
+        friendArray.sort(function (a, b) {
+          let nameA = a.lastName.toUpperCase()
+          let nameB = b.lastName.toUpperCase()
+          if (nameA < nameB) {
+            return -1
+          }
+          if (nameA > nameB) {
+            return 1
+          }
+          return 0
+        });
+        console.log(friendArray)
+      },
+      error: function (e) {
+        console.log("error", error)
+      }
     });
+    $("a").removeClass("active")
+    $(e.target).addClass("active");
+    reset();
+    e.target.children[1].classList.remove("hidden");
+    e.target.children[0].classList.add("hidden");
+    event.preventDefault();
+    let hash = this.hash;
+    $('main')
+      .animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function () {
+        window.location.hash = hash;
+      });
   });
-
-
-
+});
