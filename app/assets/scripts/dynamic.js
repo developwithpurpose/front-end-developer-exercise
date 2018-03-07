476d7096d309c20721caeda373cb6d622086b58e
Chase Steely <chasesteely@gmail.com>
2018-03-07 16:11:58 -0600

    AFTER DUE DATE EDIT refactored code to make it more efficient and dry.

--- app/assets/scripts/dynamic.js
+++ app/assets/scripts/dynamic.js
@@ -1,73 +1,43 @@
-let friendArray = []
-$.ajax({
-    url: "../../baby-steps.json",
-    type: 'get',
-    dataType: 'json',
-    success: function (data) {
-        $.each(data.friends, function (index, item) {
-            friendArray.push(item)
-        });
-        friendArray.sort(function (a, b) {
-            let nameA = a.lastName.toUpperCase()
-            let nameB = b.lastName.toUpperCase()
-            if (nameA < nameB) {
-                return -1
-            }
-            if (nameA > nameB) {
-                return 1
-            }
-            return 0
-        });
-    },
-    error: function (e) {
-        console.log("error", error)
-    }
-});
+function populateFriends(step) {
+    $.ajax({
+        url: "../../baby-steps.json",
+        type: 'get',
+        dataType: 'json',
+        success: function (data) {
+            let dContent = ''
+            let friend = data.friends
+                .filter((f) => {
+                    return f.babyStep === step
+                })
+                .sort((a, b) => {
+                    return a.lastName < b.lastName ? -1 : 1
+                })
+            switch (friend.length) {
+                case 0:
+                    break
+                case 1:
+                    dContent =
+                        `<p><a>${friend[0].firstName} ${friend[0].lastName}</a> is also in Baby Step ${friend[0].babyStep}</p>`
+                    break
 
+                case 2:
+                    dContent =
+                        ` <p><a>${friend[0].firstName} ${friend[0].lastName}</a> and <a>${friend[1].firstName} ${friend[1].lastName}</a> are also in Baby Step ${friend[0].babyStep}</p>`
+                    break
+
+                default:
+                    dContent =
+                        `<p><a>${friend[0].firstName} ${friend[0].lastName}</a>, <a>${friend[1].firstName} ${friend[1].lastName}</a>, and ${friend.length - 2} other friends are also in Baby Step ${friend[0].babyStep}</p>`
+                    break
+            }
+            $('.dynamic').html(dContent);
+        },
+        error: function (e) {
+            console.log("error", error)
+        }
+    })
+}
 
 $('a[href^="#"]').on('click', function (e) {
-    if ($("a[href~='two']")) {
-        let friend = friendArray.filter(f => f.babyStep === 2)
-        friend.forEach(function (f) {
-            two.innerHTML =
-                `
-   <p><a>${f.firstName} ${f.lastName}</a> is also in Baby Step ${f.babyStep}</p>
-   `
-        })
-    }
-    if ($("a[href~='three']")) {
-        let friend = friendArray.filter(f => f.babyStep === 3)
-        three.innerHTML =
-            `
-   <p><a>${friend[0].firstName} ${friend[0].lastName}</a> and <a>${friend[1].firstName} ${friend[1].lastName}</a> are also in Baby Step ${friend[0].babyStep}</p>
-   `
-    }
-    if ($("a[href~='four']")) {
-        let friend = friendArray.filter(f => f.babyStep === 4)
-        four.innerHTML =
-            `
-   <p><a>${friend[0].firstName} ${friend[0].lastName}</a>, <a>${friend[1].firstName} ${friend[1].lastName}</a>, and ${friend.length - 2} other friend are also in Baby Step ${friend[0].babyStep}</p>
-   `
-    }
-    if ($("a[href~='five']")) {
-        let friend = friendArray.filter(f => f.babyStep === 5)
-        five.innerHTML =
-            `
-   <p><a>${friend[0].firstName} ${friend[0].lastName}</a>, <a>${friend[1].firstName} ${friend[1].lastName}</a>, and ${friend.length - 2} other friends are also in Baby Step ${friend[0].babyStep}</p>
-   `
-    }
-    if ($("a[href~='six']")) {
-        let friend = friendArray.filter(f => f.babyStep === 6)
-        six.innerHTML =
-            `
-   <p><a>${friend[0].firstName} ${friend[0].lastName}</a>, <a>${friend[1].firstName} ${friend[1].lastName}</a>, and ${friend.length - 2} other friends are also in Baby Step ${friend[0].babyStep}</p>
-   `
-    }
-    if ($("a[href~='seven']")) {
-        let friend = friendArray.filter(f => f.babyStep === 7)
-        seven.innerHTML =
-            `
-   <p><a>${friend[0].firstName} ${friend[0].lastName}</a>, <a>${friend[1].firstName} ${friend[1].lastName}</a>, and ${friend.length - 2} other friends are also in Baby Step ${friend[0].babyStep}</p>
-   `
-    }
-});
+    populateFriends(parseInt(e.target.id))
+})
--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -4,13 +4,18 @@ $(document).ready(function () {
     $('a>img:nth-child(odd)').removeClass("hidden");
   }
 
+  $('#mainNav > li:nth-child(1) > a').addClass("active")
+  $('#mainNav > li:nth-child(1) > a').addClass("active")
+  $('#mainNav > li:nth-child(1) > a >img:nth-child(odd)').addClass("hidden");
+  $('#mainNav > li:nth-child(1) > a >img:nth-child(even)').removeClass("hidden");
+
   $('a[href^="#"]').on('click', function (e) {
-    $("a").removeClass("active")
-    $(e.target).addClass("active");
-    reset();
-    e.target.children[1].classList.remove("hidden");
-    e.target.children[0].classList.add("hidden");
     event.preventDefault();
+    $("a").removeClass("active")
+    $(this).addClass("active")
+    reset();
+    this.children[1].classList.remove("hidden");
+    this.children[0].classList.add("hidden");
     let hash = this.hash;
     $('main')
       .animate({
