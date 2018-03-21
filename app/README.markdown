a8474f61fda90dc62d6c83ba8a7ca563f4908d59
Stephen Anthony <stephenba4@gmail.com>
2018-03-20 21:11:01 -0400

    Completed project for Stephen Anthony

--- app/README.markdown
+++ app/README.markdown
@@ -1,1 +1,13 @@
-Document your submission here (optional, but recommended)
+I have created a 7 Baby Steps web page with the  requirements. Given a 4 hour time span to complete the project, there is still much room left for improvement. The project has attempted several requirements as provided in the documentation such as:
+1. I have written this web site using JavaScript, jQuery, CSS, and HTML.
+2. The website utilizes the provided assets, such as a header image and navigation bar icons.
+3. There is an animation when navigating between each of the baby steps.
+4. It makes use of a Node.js web server and can be deployed on localhost:8080.
+5. When each Baby Step is clicked, you will see which friends are also on it. This capability is made possible using AJAX and JSON.
+6. The mockup has been consulted, and given the strict timeframe of completion, only some of the styling has been completed. The CSS can be expounded upon to give the website a much more complete feel.
+7. The body copy of only 1 Baby Step will show at a time, while the others will be hidden.
+8. There are bugs that will need to be corrected, such as the first click on Baby Step 1 is not recognized. Another bug is that the animation is jittery, and needs styling.
+9. Per the documentation, the website is not responsive.
+10. The JavaScript code needs to be rewritten in a more concise manner to give the documents better code organization. DRY principles could be implemented to achieve this.
+11. A Single Page Application framework was not used to complete this web site.
+12. No comments were used. All comments are kept here in the README file.
--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,1 +1,167 @@
-/* This is the JavaScript */
+function showJSON2 () {
+  var showjson = document.getElementById("step2");
+  $.getJSON( "././baby-steps.json", function( json ) {
+    var username1 = json.friends[0].firstName + " " + json.friends[0].lastName;
+    showjson.innerHTML = username1 + " is also in Baby Step 2";
+  });
+};
+
+function showJSON3 () {
+  var showjson = document.getElementById("step3");
+  $.getJSON( "././baby-steps.json", function( json ) {
+    var username1 = json.friends[2].firstName + " " + json.friends[2].lastName;
+    var username2 = json.friends[1].firstName + " " + json.friends[1].lastName;
+    showjson.innerHTML = username1 + " and " + username2 + " are also in Baby Step 3";
+  });
+};
+
+function showJSON4 () {
+  var showjson = document.getElementById("step4");
+  $.getJSON( "././baby-steps.json", function( json ) {
+    var username1 = json.friends[3].firstName + " " + json.friends[3].lastName;
+    var username2 = json.friends[5].firstName + " " + json.friends[5].lastName;
+    showjson.innerHTML = username1 + ", " + username2 + ", and 1 other friend are also in Baby Step 4";
+  });
+};
+
+function showJSON5 () {
+  var showjson = document.getElementById("step5");
+  $.getJSON( "././baby-steps.json", function( json ) {
+    var username1 = json.friends[9].firstName + " " + json.friends[9].lastName;
+    var username2 = json.friends[8].firstName + " " + json.friends[8].lastName;
+    showjson.innerHTML = username1 + ", " + username2 + ", and 2 other friends are also in Baby Step 5";
+  });
+};
+
+function myFunction1() {
+  var div1 = document.getElementById("myDIV1");
+  if (div1.style.display === "none") {
+        div1.style.display = "block";
+    };
+  var div2 = document.getElementById("myDIV2")
+      div2.style.display = "none";
+  var div3 = document.getElementById("myDIV3")
+      div3.style.display = "none";
+  var div2 = document.getElementById("myDIV4")
+      div2.style.display = "none";
+  var div3 = document.getElementById("myDIV5")
+      div3.style.display = "none";
+  var div2 = document.getElementById("myDIV6")
+      div2.style.display = "none";
+  var div3 = document.getElementById("myDIV7")
+      div3.style.display = "none";
+};
+
+function myFunction2() {
+  var div2 = document.getElementById("myDIV2");
+  if (div2.style.display === "none") {
+    div2.style.display = "block";
+  };
+  var div1 = document.getElementById("myDIV1")
+      div1.style.display = "none";
+  var div3 = document.getElementById("myDIV3")
+      div3.style.display = "none";
+  var div4 = document.getElementById("myDIV4")
+      div4.style.display = "none";
+  var div5 = document.getElementById("myDIV5")
+      div5.style.display = "none";
+  var div6 = document.getElementById("myDIV6")
+      div6.style.display = "none";
+  var div7 = document.getElementById("myDIV7")
+      div7.style.display = "none";
+};
+
+function myFunction3() {
+  var div3 = document.getElementById("myDIV3");
+  if (div3.style.display === "none") {
+    div3.style.display = "block";
+  };
+  var div1 = document.getElementById("myDIV1")
+      div1.style.display = "none";
+  var div2 = document.getElementById("myDIV2")
+      div2.style.display = "none";
+  var div4 = document.getElementById("myDIV4")
+      div4.style.display = "none";
+  var div5 = document.getElementById("myDIV5")
+      div5.style.display = "none";
+  var div6 = document.getElementById("myDIV6")
+      div6.style.display = "none";
+  var div7 = document.getElementById("myDIV7")
+      div7.style.display = "none";
+};
+
+function myFunction4() {
+  var div4 = document.getElementById("myDIV4");
+  if (div4.style.display === "none") {
+      div4.style.display = "block";
+    };
+  var div1 = document.getElementById("myDIV1")
+      div1.style.display = "none";
+  var div2 = document.getElementById("myDIV2")
+      div2.style.display = "none";
+  var div3 = document.getElementById("myDIV3")
+      div3.style.display = "none";
+  var div5 = document.getElementById("myDIV5")
+      div5.style.display = "none";
+  var div6 = document.getElementById("myDIV6")
+      div6.style.display = "none";
+  var div7 = document.getElementById("myDIV7")
+      div7.style.display = "none";
+};
+
+function myFunction5() {
+  var div5 = document.getElementById("myDIV5");
+  if (div5.style.display === "none") {
+    div5.style.display = "block";
+  };
+  var div1 = document.getElementById("myDIV1")
+      div1.style.display = "none";
+  var div2 = document.getElementById("myDIV2")
+      div2.style.display = "none";
+  var div3 = document.getElementById("myDIV3")
+      div3.style.display = "none";
+  var div4 = document.getElementById("myDIV4")
+      div4.style.display = "none";
+  var div6 = document.getElementById("myDIV6")
+      div6.style.display = "none";
+  var div7 = document.getElementById("myDIV7")
+      div7.style.display = "none";
+};
+
+function myFunction6() {
+  var div6 = document.getElementById("myDIV6");
+  if (div6.style.display === "none") {
+    div6.style.display = "block";
+  };
+  var div1 = document.getElementById("myDIV1")
+      div1.style.display = "none";
+  var div2 = document.getElementById("myDIV2")
+      div2.style.display = "none";
+  var div3 = document.getElementById("myDIV3")
+      div3.style.display = "none";
+  var div4 = document.getElementById("myDIV4")
+      div4.style.display = "none";
+  var div5 = document.getElementById("myDIV5")
+      div5.style.display = "none";
+  var div7 = document.getElementById("myDIV7")
+      div7.style.display = "none";
+};
+
+function myFunction7() {
+  var div7 = document.getElementById("myDIV7");
+  if (div7.style.display === "none") {
+        div7.style.display = "block";
+    };
+  var div1 = document.getElementById("myDIV1")
+      div1.style.display = "none";
+  var div2 = document.getElementById("myDIV2")
+      div2.style.display = "none";
+  var div3 = document.getElementById("myDIV3")
+      div3.style.display = "none";
+  var div4 = document.getElementById("myDIV4")
+      div4.style.display = "none";
+  var div5 = document.getElementById("myDIV5")
+      div5.style.display = "none";
+  var div6 = document.getElementById("myDIV6")
+      div6.style.display = "none";
+};
