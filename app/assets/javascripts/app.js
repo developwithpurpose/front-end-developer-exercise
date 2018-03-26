fc3ca0a7a44bbbb0dc29d77f03afcecfd32963e8
madsray <30870145+madsray@users.noreply.github.com>
2018-03-26 16:38:37 -0500

    onclick nav bar works

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -25,28 +25,85 @@
    const $five = $("#stepFive");
    const $six = $("#stepSix");
    const $seven = $("#stepSeven");
-   
+
+   const $oneC = $("#one");
+   const $twoC = $("#two");
+   const $threeC = $("#three");
+   const $fourC = $("#four");
+   const $fiveC = $("#five");
+   const $sixC = $("#six");
+   const $sevenC = $("#seven");
    $one.on('click', ()=>{
-     console.log("step one clicked")
+     console.log("step one clicked");
+     $oneC.show();
+     $threeC.hide();
+     $fourC.hide();
+     $fiveC.hide();
+     $sixC.hide();
+     $sevenC.hide();
+     $twoC.hide();
    });
 
    $two.on('click', ()=>{
-     console.log("step two clicked")
+     console.log("step two clicked");
+     $oneC.hide();
+     $threeC.hide();
+     $fourC.hide();
+     $fiveC.hide();
+     $sixC.hide();
+     $sevenC.hide();
+     $twoC.show();
    });
    $three.on('click', ()=>{
-     console.log("step three clicked")
+     console.log("step three clicked");
+     $oneC.hide();
+     $threeC.show();
+     $fourC.hide();
+     $fiveC.hide();
+     $sixC.hide();
+     $sevenC.hide();
+     $twoC.hide();
    });
    $four.on('click', ()=>{
      console.log("step four clicked")
+     $oneC.hide();
+     $threeC.hide();
+     $fourC.show();
+     $fiveC.hide();
+     $sixC.hide();
+     $sevenC.hide();
+     $twoC.hide();
    });
    $five.on('click', ()=>{
      console.log("step five clicked")
+     $oneC.hide();
+     $threeC.hide();
+     $fourC.hide();
+     $fiveC.show();
+     $sixC.hide();
+     $sevenC.hide();
+     $twoC.hide();
    });
    $six.on('click', ()=>{
      console.log("step six clicked")
+     $oneC.hide();
+     $threeC.hide();
+     $fourC.hide();
+     $fiveC.hide();
+     $sixC.show();
+     $sevenC.hide();
+     $twoC.hide();
    });
    $seven.on('click', ()=>{
-     console.log("step seven clicked")
+     console.log("step seven clicked");
+     $sevenC.show();
+     $oneC.hide();
+     $threeC.hide();
+     $fourC.hide();
+     $fiveC.hide();
+     $sixC.hide();
+    
+     $twoC.hide();
    });
 
  });
