e8daf01fd7c19259143b09f86f6d984dce84cc98
madsray <30870145+madsray@users.noreply.github.com>
2018-03-26 19:01:52 -0500

    friends are added

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,29 @@
 
+ $(()=>{
+    const arr1 = [];
+    const arr2 = [];
+    const arr3 = [];
+    const arr4 = [];
+    const arr5 = [];
+    const arr6 = [];
+    const arr7 = [];
+    const subArr = [];
+    const $one = $("#stepOne");
+    const $two = $("#stepTwo");
+    const $three = $("#stepThree");
+    const $four = $("#stepFour");
+    const $five = $("#stepFive");
+    const $six = $("#stepSix");
+    const $seven = $("#stepSeven");
+
+    const $oneC = $("#one");
+    const $twoC = $("#two");
+    const $threeC = $("#three");
+    const $fourC = $("#four");
+    const $fiveC = $("#five");
+    const $sixC = $("#six");
+    const $sevenC = $("#seven");
+
    function loadJSON(callback) {
 
       var request = new XMLHttpRequest();
@@ -13,26 +38,107 @@
    }
 
  loadJSON(function(response) {
-    var json_data = JSON.parse(response);
-    console.log(json_data);
+    var data = JSON.parse(response);
+    let arr = data.friends;
+    for (let i = 0; i<arr.length; i++ ){
+
+        if ( arr[i].babyStep === 1){
+          let people1 = arr[i]
+          arr1.push(people1);
+          if (arr1.length === 1){
+            $('#p1').text(arr1[0].firstName + " " + arr1[0].lastName + " is also in Baby Step 1");
+          } else if (arr1.length === 2){
+            $('#p1').text(arr1[0].firstName  + " " +arr1[0].lastName + " and " + arr1[1].firstName  + " " + arr1[1].lastName +  " are also in Baby Step 1");
+          } else if (arr1.length === 3){
+            $('#p1').text(arr1[0].firstName + " " +  arr1[0].lastName + ", " + arr1[1].firstName + " " + arr1[1].lastName + " and " + " 1 other friend are also in Baby Step 1");
+          }else if (arr1.length >= 4){
+            $('#p1').text(arr1[0].firstName + " " + arr1[0].lastName + ", " + arr1[1].firstName + arr1[1].lastName + ", " + arr1[2].firstName+" "  + arr1[2].lastName + " and 2 other friends are also in Baby Step 1");
+          };
+
+
+        } else if ( arr[i].babyStep === 2){
+          let people2 = arr[i]
+          arr2.push(people2);
+          if (arr2.length === 1){
+            $('#p2').text(arr2[0].firstName + " " + arr2[0].lastName + " is also in Baby Step 1");
+          } else if (arr2.length === 2){
+            $('#p2').text(arr1[0].firstName +  " " +arr1[0].lastName + " and " + arr2[1].firstName +  " " +arr2[1].lastName +  " are also in Baby Step 1");
+          } else if (arr2.length === 3){
+            $('#p2').text(arr2[0].firstName + " " + arr2[0].lastName + ", " + arr[2].firstName +  " " +arr2[1].lastName + " and " + " 1 other friend are also in Baby Step 1");
+          }else if (arr2.length >= 4){
+            $('#p2').text(arr1[0].firstName + " " + arr2[0].lastName + ", " + arr2[1].firstName +  " " +arr2[1].lastName + ", " + arr2[2].firstName +  " " +arr2[2].lastName + " and 2 other friends are also in Baby Step 1");
+          };
+
+        }else if ( arr[i].babyStep === 3){
+          let people3 = arr[i]
+          arr3.push(people3);
+          if (arr3.length === 1){
+            $('#p3').text(arr3[0].firstName + " " + arr3[0].lastName + " is also in Baby Step 1");
+          } else if (arr3.length === 2){
+            $('#p3').text(arr3[0].firstName + " " + arr3[0].lastName + " and " + arr3[1].firstName +  " " +arr3[1].lastName +  " are also in Baby Step 1");
+          } else if (arr3.length === 3){
+            $('#p3').text(arr3[0].firstName + " " + arr3[0].lastName + ", " + arr3[1].firstName +  " " +arr3[1].lastName + " and " + " 1 other friend are also in Baby Step 1");
+          }else if (arr3.length >= 4){
+            $('#p3').text(arr3[0].firstName +  " " +arr3[0].lastName + ", " + arr3[1].firstName +  " " +arr3[1].lastName + ", " + arr3[2].firstName + " " + arr3[2].lastName + " and 2 other friends are also in Baby Step 1");
+          };
+
+
+        }else if ( arr[i].babyStep === 4){
+          let people4 = arr[i]
+          arr4.push(people4);
+          if (arr4.length ===1){
+            $('#p4').text(arr4[0].firstName + " " + arr4[0].lastName + " is also in Baby Step 1");
+          } else if (arr4.length === 2){
+            $('#p4').text(arr4[0].firstName +  " " +arr4[0].lastName + " and " + arr4[1].firstName +  " " +arr4[1].lastName +  " are also in Baby Step 1");
+          } else if (arr4.length === 3){
+            $('#p4').text(arr4[0].firstName +  " " +arr4[0].lastName + ", " + arr4[1].firstName +  " " +arr4[1].lastName + " and " + " 1 other friend are also in Baby Step 1");
+          }else if (arr4.length >= 4){
+            $('#p4').text(arr4[0].firstName +  " " +arr4[0].lastName + ", " + arr4[1].firstName +  " " +arr4[1].lastName + ", " + arr4[2].firstName +  " " +arr4[2].lastName + " and 2 other friends are also in Baby Step 1");
+          };
+
+        }else if ( arr[i].babyStep === 5){
+          let people5 = arr[i]
+          arr5.push(people5);
+          if (arr5.length === 1){
+            $('#p5').text(arr5[0].firstName + " " + arr5[0].lastName + " is also in Baby Step 1");
+          } else if (arr5.length === 2){
+            $('#p5').text(arr5[0].firstName +  " " +arr5[0].lastName + " and " + arr5[1].firstName +  " " +arr5[1].lastName +  " are also in Baby Step 1");
+          } else if (arr5.length === 3){
+            $('#p5').text(arr5[0].firstName +  " " +arr5[0].lastName + ", " + arr5[1].firstName + " " + arr5[1].lastName + " and " + " 1 other friend are also in Baby Step 1");
+          }else if (arr5.length >= 4){
+            $('#p5').text(arr5[0].firstName +  " " +arr5[0].lastName + ", " + arr5[1].firstName +  " " +arr5[1].lastName + ", " + arr5[2].firstName +  " " +arr5[2].lastName + " and 2 other friends are also in Baby Step 1");
+          };
+
+        }else if ( arr[i].babyStep === 6){
+          let people6 = arr[i]
+          arr6.push(people6);
+          if (arr6.length === 1){
+            $('#p6').text(arr6[0].firstName + " " + arr6[0].lastName + " is also in Baby Step 1");
+          } else if (arr6.length === 2){
+            $('#p6').text(arr6[0].firstName +  " " +arr6[0].lastName + " and " + arr6[1].firstName + " " + arr6[1].lastName +  " are also in Baby Step 1");
+          } else if (arr6.length === 3){
+            $('#p6').text(arr6[0].firstName +  " " +arr6[0].lastName + ", " + arr6[1].firstName +  " " +arr6[1].lastName + " and " + " 1 other friend are also in Baby Step 1");
+          }else if (arr6.length >= 4){
+            $('#p6').text(arr6[0].firstName + arr6[0].lastName + ", " + arr6[1].firstName +  " " +arr6[1].lastName + ", " + arr6[2].firstName +  " " +arr6[2].lastName + " and 2 other friends are also in Baby Step 1");
+          };
+
+        }else if ( arr[i].babyStep === 7){
+          let people7 = arr[i]
+          arr7.push(people7);
+          if (arr7.length === 1){
+            $('#p7').text(arr7[0].firstName + " " + arr7[0].lastName + " is also in Baby Step 1");
+          } else if (arr7.length === 2){
+            $('#p7').text(arr7[0].firstName +  " " +arr7[0].lastName + " and " + arr7[1].firstName +  " " +arr7[1].lastName +  " are also in Baby Step 1");
+          } else if (arr7.length === 3){
+            $('#p7').text(arr7[0].firstName +  " " +arr7[0].lastName + ", " + arr7[1].firstName +  " " +arr7[1].lastName + " and " + " 1 other friend are also in Baby Step 1");
+          }else if (arr7.length >= 4){
+            $('#p7').text(arr7[0].firstName +  " " +arr7[0].lastName + ", " + arr7[1].firstName +  " " +arr7[1].lastName + ", " + arr7[2].firstName +  " " +arr7[2].lastName + " and 2 other friends are also in Baby Step 1");
+          };
+        }
+
+}
  });
 
- $(()=>{
-   const $one = $("#stepOne");
-   const $two = $("#stepTwo");
-   const $three = $("#stepThree");
-   const $four = $("#stepFour");
-   const $five = $("#stepFive");
-   const $six = $("#stepSix");
-   const $seven = $("#stepSeven");
-
-   const $oneC = $("#one");
-   const $twoC = $("#two");
-   const $threeC = $("#three");
-   const $fourC = $("#four");
-   const $fiveC = $("#five");
-   const $sixC = $("#six");
-   const $sevenC = $("#seven");
    $one.on('click', ()=>{
      console.log("step one clicked");
      $oneC.show();
@@ -45,6 +151,9 @@
      $four.find('img').attr('src','./assets/images/icons/individual/icons_large_bs4.png');
      $five.find('img').attr('src','./assets/images/icons/individual/icons_large_bs5.png');
      $six.find('img').attr('src','./assets/images/icons/individual/icons_large_bs6.png');
+     // if (arr1.length === 1 ){
+     //   $('#pOne').text(arr1.firstName + arr1.lastName +" is also in Baby Step 1");
+     // }
    });
 
    $two.on('click', ()=>{
