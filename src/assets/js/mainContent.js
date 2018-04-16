bc4943b0be30fa3cc913d51fbde73f6f46f45229
Jonathan Melin <melin.jonathan@gmail.com>
2018-04-15 21:07:56 -0400

    alphabetize names

--- src/assets/js/mainContent.js
+++ src/assets/js/mainContent.js
@@ -7,30 +7,36 @@ const mainContent = document.querySelector('.js-content')
 mainContent.innerHTML = content.map((step, i) => {
   let classList = 'c-step-content js-step-content'
   let totalFriends = []
-  let friendsContent = '';
+  let friendsContent = ''
   
   // Make friends content
   friends.forEach(friend => {
     if (friend.babyStep === i+1) totalFriends.push(friend)
-
-    const name1 = totalFriends.length >= 1 ? 
-                  `<a>${totalFriends[0].firstName} ${totalFriends[0].lastName}</a>` :
-                  ''
-    const name2 = totalFriends.length >= 2 ? 
-                  `<a>${totalFriends[1].firstName} ${totalFriends[1].lastName}</a>` :
-                  ''
-    const ending = `also in Baby Step ${i+1}`
-
-    if (totalFriends.length === 1) {
-      friendsContent = `${name1} is ${ending}`
-    } else if (totalFriends.length === 2) {
-      friendsContent = `${name1} and ${name2} are ${ending}`
-    } else if (totalFriends.length === 3) {
-      friendsContent = `${name1}, ${name2} and 1 other friend is ${ending}`
-    } else if (totalFriends.length >= 4) {
-      friendsContent = `${name1}, ${name2} and ${totalFriends.length - 2} other friends are ${ending}`
-    }
   })
+
+  const alphaNames = totalFriends.sort((prev, next) => {
+    let [a, b] = [prev.lastName, next.lastName]
+    if (a === b) [a, b] = [prev.firstName, next.firstName]
+    return a > b ? 1 : -1
+  })
+
+  const name1 = alphaNames.length >= 1 ? 
+                `<a>${alphaNames[0].firstName} ${alphaNames[0].lastName}</a>` :
+                ''
+  const name2 = alphaNames.length >= 2 ? 
+                `<a>${alphaNames[1].firstName} ${alphaNames[1].lastName}</a>` :
+                ''
+  const ending = `also in Baby Step ${i+1}`
+
+  if (alphaNames.length === 1) {
+    friendsContent = `${name1} is ${ending}`
+  } else if (alphaNames.length === 2) {
+    friendsContent = `${name1} and ${name2} are ${ending}`
+  } else if (alphaNames.length === 3) {
+    friendsContent = `${name1}, ${name2} and 1 other friend is ${ending}`
+  } else if (alphaNames.length >= 4) {
+    friendsContent = `${name1}, ${name2} and ${alphaNames.length - 2} other friends are ${ending}`
+  }
 
   if (i === 0) {
     classList = 'c-step-content c-step-content--active js-step-content' 
