a02222ec4e912120743b295f698787a45dd3c356
cjharkins <charkins@nossi.edu>
2018-05-03 21:44:29 -0500

    created sorted friends list

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,7 +7,6 @@ $('document').ready(function(){
 	const offset 			 = 5;
 	
 	$('a[href^="#"]').on('click',function(e){
-		// e.preventDefault();
 
 		let target 			 = this.hash;
 		let $target      = $(target);
@@ -34,24 +33,20 @@ $('document').ready(function(){
 			case '#one':
 				toggleMe(img);
 				toggleBar.animate({'top': base + 'px'},100);
-				// $('.parent-conceal').animate({'scrollTop': 0} ,1000, 'swing');
 				break;
 			case '#two':		
 				toggleMe(img);
 				toggleBar.animate({'top': base + initialCount + 'px'},100);
-				// $('.parent-conceal').animate({'scrollTop': 0 },1000, 'swing');
 				break;
 			case '#three':	
 				toggleMe(img);
 				currentCount = (initialCount * 2) + offset;
 				toggleBar.animate({'top': base+currentCount+'px'},100);
-				// $('.parent-conceal').animate({'scrollTop': 0 },1000, 'swing');
 				break;
 			case '#four':	
 				toggleMe(img);
 				currentCount = (initialCount * 3) + (offset * 2);
 				toggleBar.animate({'top': base+currentCount+'px'},100);
-				// $('.parent-conceal').animate({'scrollTop':0},1000, 'swing');
 				break;
 			case '#five':		
 				toggleMe(img);
@@ -75,12 +70,78 @@ $('document').ready(function(){
 
   $.ajax({
     type: 'GET',
-    url: 'http://localhost:8080/./app/baby-steps.json',
+    url: '/baby-steps.json',
     success: function(data) {
-      console.log(data);
+      let step1 = [], step2 = [], step3 = [], step4 = [], step5 = [], step6 = [], step7 = [];
+      let steps = [step1,step2,step3,step4,step5,step6,step7];
+      for (var i = 0; i < data.friends.length;i++){
+        let step = data.friends[i].babyStep;
+        if ( step === 1) {
+          step1.push(data.friends[i]);
+        } else if ( step === 2){
+          step2.push(data.friends[i]);
+        } else if (step === 3) {
+          step3.push(data.friends[i]); 
+        } else if (step === 4) {
+          step4.push(data.friends[i]);
+        } else if (step === 5) {
+          step5.push(data.friends[i]);
+        } else if (step === 6) {
+          step6.push(data.friends[i]);
+        } else if (step === 7) {
+          step7.push(data.friends[i]);
+        } else {
+          return;
+        }
+      }
+      function compare(a,b) {
+        const lastNameA = a.lastName.toUpperCase();
+        const lastNameB = b.lastName.toUpperCase();
+        let comparison = 0;
+
+        lastNameA > lastNameB ? comparison = 1:comparison = -1;
+
+        if (comparison == 0) {
+          const firstNameA = a.firstName.toUpperCase();
+          const firstNameB = b.firstName.toUpperCase();
+          let comparison = 0;
+
+          firstNameA > firstNameB ? comparison = 1:comparison = -1;
+
+        }
+        
+        return comparison;
+      }
+
+      step1.sort(compare);
+      step2.sort(compare);
+      step3.sort(compare);
+      step4.sort(compare);
+      step5.sort(compare);
+      step6.sort(compare);
+      step7.sort(compare);
+      
+      function twoOrMore(arr){
+        const html = `<p><a href="#">${arr[0].firstName} ${arr[0].lastName}</a>,
+                         <a href="#">${arr[1].firstName} ${arr[1].lastName}</a> 
+                              and ${arr.length - 2} others are on this step.</p>`;
+        return html;
+      }
+
+      function lessThanTwo(arr) {
+        const html = `<p><a href="#">
+                      ${arr[0].firstName} ${arr[0].lastName}
+                    </a> is on this step.</p>`;
+        return html;
+      }
+
+      step1.length >= 2? $('#one').append(twoOrMore(step1)):step1.length >= 1? $('#one').append(lessThanTwo(step1)):$('#one').append('<p>No friends are on this step.<p>');
+      step2.length >= 2? $('#two').append(twoOrMore(step2)):step2.length >= 1? $('#two').append(lessThanTwo(step2)):$('#two').append('<p>No friends are on this step.<p>');
+      step3.length >= 2? $('#three').append(twoOrMore(step3)):step3.length >= 1? $('#three').append(lessThanTwo(step3)):$('#three').append('<p>No friends are on this step.<p>');
+      step4.length >= 2? $('#four').append(twoOrMore(step4)):step4.length >= 1? $('#four').append(lessThanTwo(step4)):$('#four').append('<p>No friends are on this step.<p>');
+      step5.length >= 2? $('#five').append(twoOrMore(step5)):step5.length >= 1? $('#five').append(lessThanTwo(step5)):$('#five').append('<p>No friends are on this step.<p>');
+      step6.length >= 2? $('#six').append(twoOrMore(step6)):step6.length >= 1? $('#six').append(lessThanTwo(step6)):$('#six').append('<p>No friends are on this step.<p>');
+      step7.length >= 2? $('#seven').append(twoOrMore(step7)):step7.length >= 1? $('#seven').append(lessThanTwo(step7)):$('#seven').append('<p>No friends are on this step.<p>');
     }
   });
-
-
-
 });
