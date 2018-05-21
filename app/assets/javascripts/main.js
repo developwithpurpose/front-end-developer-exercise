23cdad9fb09154f8a28ff0a175e7607763087bd7
John Dulaney <john.s.dulaney1@gmail.com>
2018-05-20 19:32:26 -0500

    :construction: dug into json array, sorted by babystep into individual arrays.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,79 +1,98 @@
-const stepItems = $('.stepItem');
-
-for (let i = 0; i < stepItems.length; i++) {
-  const currentStepItem = stepItems[i];
-  const currentStepItemHeight =
-    $(currentStepItem).offset().top + $(currentStepItem).height();
-
-  $(window).on('scroll', function() {
-    let stop = Math.round($(window).scrollTop());
-    if (stop > currentStepItemHeight) {
-      $('.nav').addClass('active');
-      console.log('scrolled past ', currentStepItem);
-    } else {
-      $('.nav').removeClass('active');
-      console.log('not past ', currentStepItem);
-    }
-  });
-}
-
 // friend zone
 const getData = () => {
-  fetch('../../baby-steps.json')
-    .then(
-      (response => {
-        if (response.status !== 200) {
-          console.log(
-            'Looks like there was an unexpected response. Status Code: ' +
-              response.status
-          );
-          return;
-        }
-        response.json().then(
-          (r = data => {
-            dom(data)
-          })
+  fetch('../../baby-steps.json', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      if (response.status !== 200) {
+        console.log(
+          'Looks like there was an unexpected response. Status Code: ' +
+          response.status
         );
-      })
-    )
-    .catch(function(err) {
+        return;
+      }
+      response.json().then(
+        (r = data => {
+          dom(data);
+        })
+      );
+    })
+    .catch(function (err) {
       console.log('Fetch Error, check the function: ', err);
     });
 };
 getData();
 
-const dom = response => {
-  // console.log(response)
-  let lazy = response.friends
-  console.log(lazy[0])
-  $('friend__Status').html(`${lazy[0].firstName}`);
+const dom = (response, divisions) => {
+  const friends = response.friends,
+    stepOne = [],
+    stepTwo = [],
+    stepThree = [],
+    stepFour = [],
+    stepFive = [],
+    stepSix = [],
+    stepSeven = [];
+    divisions = [stepOne, stepTwo, stepThree, stepFour, stepFive, stepSix, stepSeven];
+
+  friends.forEach(x => {
+    if (x.babyStep === 1) {
+      stepOne.push(x);
+    } else if (x.babyStep === 2) {
+      stepTwo.push(x);
+    } else if (x.babyStep === 3) {
+      stepThree.push(x);
+    } else if (x.babyStep === 4) {
+      stepFour.push(x);
+    } else if (x.babyStep === 5) {
+      stepFive.push(x);
+    } else if (x.babyStep === 6) {
+      stepSix.push(x);
+    } else if (x.babyStep === 7) {
+      stepSeven.push(x);
+    } else {
+      return console.log(
+        "Error occurred parsing friends' current steps: " +
+        x.babyStep +
+        ' has no matching div.'
+      );
+    }
+  });
+  pushToDivs(divisions)
 };
 
-// const stepItems = document.querySelectorAll('.stepItem');
+const pushToDivs = divisions => {
+  divisions.forEach(friendArray => {
+    friendArray.forEach(currentFriend => {
+      const string = `${currentFriend.firstName} ${currentFriend.lastName} is on step ${currentFriend.babyStep}`;
+      
+    })
 
-// let currentstepItem = 0;
 
-// const nextstepItem = () => {
-//   goTostepItem(currentstepItem + 1);
+  })
+
+}
+
+
+// const interpolation = (divTwo, currentFriend) => {
+//   console.log(divTwo[0]);
 // };
 
-// const previousstepItem = () => {
-//   goTostepItem(currentstepItem - 1);
-// };
+// for (let i = 0; i < stepItems.length; i++) {
+//   const currentStepItem = stepItems[i];
+//   const currentStepItemHeight =
+//     $(currentStepItem).offset().top + $(currentStepItem).height();
 
-// const goTostepItem = n => {
-//   stepItems[currentstepItem].className = 'stepItem';
-//   currentstepItem = (n + stepItems.length) % stepItems.length;
-//   stepItems[currentstepItem].className = 'stepItem showing';
-// };
-
-// document.scrollTop = event => {
-//   switch (event.keyCode) {
-//     case 37:
-//       previousstepItem();
-//       break;
-//     case 39:
-//       nextstepItem();
-//       break;
-//   }
-// };
+//   $(window).on('scroll', function() {
+//     let stop = Math.round($(window).scrollTop());
+//     if (stop > currentStepItemHeight) {
+//       $('.nav').addClass('active');
+//       console.log('scrolled past ', currentStepItem);
+//     } else {
+//       $('.nav').removeClass('active');
+//       console.log('not past ', currentStepItem);
+//     }
+//   });
+// }
