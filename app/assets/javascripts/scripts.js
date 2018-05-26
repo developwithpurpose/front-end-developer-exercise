0daf97df2ab0838b860a3b75ae026d1017f5bdb6
coreyderosa <coreyderosa@gmail.com>
2018-05-26 17:02:58 -0600

    Phase 2 and 3: Was able to do all requirements except for: ajax call and hide all divs but one selected. Looks clean!

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,23 +1,53 @@
-$(function(){
-    $("#baby-step-1-button").click(function(){
-        $(".baby-step-1-section").toggle();
+$(function () {
+
+    // hide all but step one on load.  No flash!
+    $("#baby-step-2-section").hide();
+    $("#baby-step-3-section").hide();
+    $("#baby-step-4-section").hide();
+    $("#baby-step-5-section").hide();
+    $("#baby-step-6-section").hide();
+    $("#baby-step-7-section").hide();
+    $("#section").hide();
+    $("section").css("display", "");
+    $("section").css("display", "block");
+    $("#baby-step-1-button").addClass("babystep-selected");
+
+    // toggle baby steps when clicked
+    $("#baby-step-1-button").click(function () {
+        $("section").css("display", "");
+        $("section").css("display", "block");
+        $("#baby-step-1-button").toggleClass("babystep-selected");
+        $("#baby-step-1-section").fadeToggle();
     });
-    $("#baby-step-2-button").click(function(){
-        $(".baby-step-2-section").toggle();
+    $("#baby-step-2-button").click(function () {
+        $("section").css("display", "");
+        $("section").css("display", "block");
+        $("#baby-step-2-button").toggleClass("babystep-selected");
+        $("#baby-step-2-section").fadeToggle();
     });
-    $("#baby-step-3-button").click(function(){
-        $(".baby-step-3-section").toggle();
+    $("#baby-step-3-button").click(function () {
+        $("section").css("display", "");
+        $("section").css("display", "block");
+        $("#baby-step-3-section").fadeToggle();
     });
-    $("#baby-step-4-button").click(function(){
-        $(".baby-step-4-section").toggle();
+    $("#baby-step-4-button").click(function () {
+        $("section").css("display", "");
+        $("section").css("display", "block");
+        $("#baby-step-4-section").fadeToggle();
     });
-    $("#baby-step-5-button").click(function(){
-        $(".baby-step-5-section").toggle();
+    $("#baby-step-5-button").click(function () {
+        $("section").css("display", "");
+        $("section").css("display", "block");
+        $("#baby-step-5-section").fadeToggle();
     });
-    $("#baby-step-6-button").click(function(){
-        $(".baby-step-6-section").toggle();
+    $("#baby-step-6-button").click(function () {
+        $("section").css("display", "");
+        $("section").css("display", "block");
+        $("#baby-step-6-section").fadeToggle();
     });
-    $("#baby-step-7-button").click(function(){
-        $(".baby-step-7-section").toggle();
+    $("#baby-step-7-button").click(function () {
+        $("section").css("display", "");
+        $("section").css("display", "block");
+        $("#baby-step-7-section").fadeToggle();
     });
 })
