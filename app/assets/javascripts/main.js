cd6ea8e862aa1a247fc142b1d07d43f775a8c463
Trevor Broaddus <tbtrevbroaddus@gmail.com>
2018-07-01 17:10:26 -0500

    AddDynamicContent make outline elements clickable

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,27 @@
 (function($) {
 
     var App = {
+        activeClass: 'active',
         init: function() {
-            $('.step-outline__step--one').addClass('active');
-            $('.step-info .step--one').addClass('active');
+            this.updateActiveStates('one');
+            this.registerListeners();
+        },
+        updateActiveStates: function(step) {
+            $('.active').each(function(index, element) {
+                $(element).removeClass(this.activeClass);
+            }.bind(this));
+
+            $('.step-outline__step--' + step).addClass(this.activeClass);
+            $('.step-info .step--' + step).addClass(this.activeClass);
+        },
+        registerListeners: function() {
+            $('.step-outline__step').click(function(event) {
+                event.preventDefault();
+                var stepNum = $(event.target).data('step');
+                this.updateActiveStates(stepNum);
+            }.bind(this));
         }
     };
 
-    $(document).ready(App.init);
+    $(document).ready(App.init.bind(App));
 })(jQuery)
