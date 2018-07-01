acef36ab2fa83113dc2509a625746be9c796a7ef
Trevor Broaddus <tbtrevbroaddus@gmail.com>
2018-07-01 16:23:11 -0500

    UseExpress Use a more sane architecture; learn some Express!

---
+++ app/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var babySteps = require('./baby-steps.json');
+var exphbs  = require('express-handlebars');
+
+var app = express ();
+var hbs = exphbs.create({});
+
+// templating
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', 'app/views');
+
+// static assets
+app.use(express.static('app'));
+
+app.get('/', function (req, res) {
+  res.render('index', babySteps);
+})
+
+app.listen('8080');
