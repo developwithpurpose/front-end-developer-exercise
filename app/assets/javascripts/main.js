8d7d351abfb096964066695a3ed4fca1eaaac88f
Trevor Broaddus <tbtrevbroaddus@gmail.com>
2018-07-01 17:18:41 -0500

    AddDynamicContent refactor the way handlers are registered

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,6 +2,20 @@
 
     var App = {
         activeClass: 'active',
+        clickables: [
+            {
+                selector: '.step-outline__step',
+                handler: 'outlineStepHandler'
+            },
+            {
+                selector: '.step-outline__step .step-name',
+                handler: 'outlineStepHandler'
+            },
+            {
+                selector: '.step-outline__step .step-logo',
+                handler: 'outlineStepHandler'
+            }
+        ],
         init: function() {
             this.updateActiveStates('one');
             this.registerListeners();
@@ -15,11 +29,23 @@
             $('.step-info .step--' + step).addClass(this.activeClass);
         },
         registerListeners: function() {
-            $('.step-outline__step').click(function(event) {
-                event.preventDefault();
-                var stepNum = $(event.target).data('step');
-                this.updateActiveStates(stepNum);
-            }.bind(this));
+            for(var index in this.clickables) {
+                var clickable = this.clickables[index];
+                if (clickable === null) {
+                    continue;
+                }
+
+                if (typeof this[clickable.handler] !== 'function') {
+                    continue;
+                }
+
+                $(clickable.selector).click(this[clickable.handler].bind(this));
+            }
+        },
+        outlineStepHandler: function(event) {
+            event.preventDefault();
+            var stepNum = $(event.target).data('step');
+            this.updateActiveStates(stepNum);
         }
     };
 
