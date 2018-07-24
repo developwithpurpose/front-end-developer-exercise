a06e71e9c295d31adf37fe0fe0655565d4723219
Rich Hanna <richhanna@MacBook-Pro-23.local>
2018-07-23 22:58:53 -0700

    All done

---
+++ app/assets/javascripts/lib.js
@@ -0,0 +1,10 @@
+var content = {
+  stepOne : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum nunc. Fermentum posuere urna nec tincidunt praesent semper. Sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Feugiat in ante metus dictum. Nibh cras pulvinar mattis nunc sed blandit libero volutpat. Nec dui nunc mattis enim ut tellus elementum sagittis. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Adipiscing elit ut aliquam purus sit. Dignissim convallis aenean et tortor at risus. Fames ac turpis egestas sed tempus urna et pharetra. Sollicitudin nibh sit amet commodo. Nunc sed blandit libero volutpat sed. Aliquam ut porttitor leo a diam sollicitudin tempor id. At elementum eu facilisis sed odio morbi quis commodo odio. Ipsum dolor sit amet consectetur adipiscing. Eget velit aliquet sagittis id consectetur. Purus sit amet volutpat consequat mauris. Libero enim sed faucibus turpis in.",
+  stepTwo : "Faucibus in ornare quam viverra orci. Aliquet enim tortor at auctor urna. Euismod quis viverra nibh cras pulvinar. Eget arcu dictum varius duis at. Morbi tristique senectus et netus et malesuada. Faucibus a pellentesque sit amet porttitor. Leo vel orci porta non. Habitant morbi tristique senectus et netus et malesuada. Est pellentesque elit ullamcorper dignissim. Id donec ultrices tincidunt arcu non. Nibh ipsum consequat nisl vel pretium lectus quam id leo."
+}
+
+function Employee () {
+  console.log("test");
+}
+
+export default "Test"
