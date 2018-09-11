be418e42ed1ee8f990161dd3dd46542436a0c37c
NuccioJohn <nuccio.john@gmail.com>
2018-09-11 15:17:31 -0700

    test
    not entirely finished

---
+++ app/assets/javascripts/GetJSON.js
@@ -0,0 +1,69 @@
+﻿         function getString(index){
+         		$.getJSON('../../baby-steps.json', function(jd) {
+                var array = [];
+                var str = "";
+                var count = 0;
+                for(var i = 0; i < jd.friends.length; i++){
+                    if(jd.friends[i].babyStep === index){
+                        array.push('<a href="#" class="blue-people">' + jd.friends[i].firstName + ' '+ jd.friends[i].lastName);
+                        count++;
+                    }
+                }
+                for(var q = 0; q < array.length; q++){
+                    if(q <3){
+                        str = str + array[q]
+                        count--;
+                        if(q < 2){
+                            str = str + ",</a> ";
+                        }else{
+                            str = str + "</a> ";
+                        }
+                    }
+                }
+                var str2 = "";
+                if(str.length > "2"){
+                	str2 = '<p class="grey-comment">and '+count+' Other Friends on this step<p>';
+                }else{
+                	str2 = '<p class="grey-comment">' + count+' Other Friends on this step<p>';
+                }
+                str = str + str2;
+
+                  $('#stage').html(str);
+               });
+         }
+
+         $(document).ready(function() {
+           	
+
+         	  $("#load1").click(function(event){
+               	getString(1)
+         	  });
+
+         	  $("#load2").click(function(event){
+               	getString(2)
+         	  });
+
+         	  $("#load3").click(function(event){
+               	getString(3)
+         	  });
+
+         	  $("#load4").click(function(event){
+               	getString(4)
+         	  });
+
+         	  $("#load5").click(function(event){
+               	getString(5)
+         	  });
+
+         	  $("#load6").click(function(event){
+               	getString(6)
+         	  });
+
+         	  $("#load7").click(function(event){
+               	getString(7)
+         	  });
+       });
+
+
+
+
---
+++ app/assets/javascripts/Slider.js
@@ -0,0 +1,41 @@
+﻿
+var babySteplOne = 1;
+
+function callJsonData(){
+
+	
+}
+
+function changeBabySteps(newIndex){
+	
+	var newPosition = ( window.panelWidth * newIndex ) * -1;
+	var newPanelHeight = $('.sp .panel:nth-child('+(newIndex+1)+')').find('.content').height() + 0;
+
+	$('.sp .items span').removeClass('selected');
+	$('.sp .items span:nth-child('+(newIndex+1)+')').addClass('selected');
+
+	$('.sp .baby-Step-inner-container').animate({left:newPosition},400);
+	$('.sp .baby-step-container').animate({height:newPanelHeight},400);
+
+}
+
+$(document).ready(function(panelWidth = 0){
+
+	window.panelWidth = $('.sp').width();
+
+	$('.baby-step-container .panel').each(function(index){
+
+		$(this).css({'width':window.panelWidth+'px','left':(index*window.panelWidth)+'px'});
+
+		$('.sp .baby-Step-inner-container').css('width',(index+1)*window.panelWidth+'px');
+
+	});
+		$('.sp .items span').each(function(){
+		$(this).on('click', function(){
+			changeBabySteps( $(this).index() );
+		});
+	});
+		$('.sp .items span:nth-child('+window.babySteplOne+')').trigger('click');
+});
+
+
