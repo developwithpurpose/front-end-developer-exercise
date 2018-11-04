5e703cd5e34adf7ff6e7f19b54a5a283ac05d44c
Justin Haight <justin@2birds1stone.com>
2018-11-03 21:31:28 -0400

    Initial Commit
    Pending Changes:
    * Update the HTML Content so Javascript isn't required.
    * Update the Divs on the Index to Hn Tags for Accessibility
    * Abstract the Javascript File

--- app/README.markdown
+++ app/README.markdown
@@ -1,1 +1,37 @@
-Document your submission here (optional, but recommended)
+## Javascript Documentation
+### navigationBabyStep(number)
+* number - baby step number
+* Starts on navigation click
+* Runs three functions: _getBabyStepContent, _getFriends, and _changeNavigation
+* Dynamically updates the page
+
+### _getBabyStepContent(number)
+* number - baby step number
+* Makes a call getting the baby step content
+* Updates the title (Baby Step 1)
+* Updates the subtitle ($1,000 Emergency Fund)
+* Updates the paragraph content for each baby step
+
+### _getFriends(number)
+* number - baby step number
+* Makes a call returning social friends list
+* Compiles the social friends string and displays to user
+* Calls _compileSocialString
+
+### _compileSocialString(names, babyStep)
+* names - names of everyone on the same baby step you are viewing
+* babySteps - current baby step you are viewing
+* Compiles the string that will be displayed to the user
+* Calls _getName
+
+### _getName(name)
+* name - A single name object passed to function
+* Adds span tag and gets the first and last name so name appears in blue
+
+## CSS
+CSS uses BEM naming conventions and has a max of two identifiers per style object
+### Hero
+* Image is defined as a background-image rather than in an img
+### Content - Navigation
+* Animation uses transition ease-out and the width of the background is 200%. The gradient is on the left half and white background is on the right
+* Navigation Images use the sprites so there is only a single network call
---
+++ app/main.js
@@ -0,0 +1,94 @@
+var currentStep = '1';
+
+function navigateBabyStep(number) {
+  _getBabyStepContent(number);
+  _getFriends(number);
+  _changeNavigation(number);
+}
+
+function _getBabyStepContent(number) {
+  var request = new XMLHttpRequest();
+  request.overrideMimeType("application/json");
+  request.open("GET", "./baby-steps-content.json", true);
+
+  request.onload = function() {
+    var jsonResponse = JSON.parse(request.responseText);
+
+    var babyStepData = {};
+    switch(number) {
+      case 1:
+      default:
+        babyStepData = jsonResponse.one;
+        break;
+      case 2:
+        babyStepData = jsonResponse.two;
+        break;
+      case 3:
+        babyStepData = jsonResponse.three;
+        break;
+      case 4:
+        babyStepData = jsonResponse.four;
+        break;
+      case 5:
+        babyStepData = jsonResponse.five;
+        break;
+      case 6:
+        babyStepData = jsonResponse.six;
+        break;
+      case 7:
+        babyStepData = jsonResponse.seven;
+    }
+    console.log("data", babyStepData);
+    document.getElementById("content-header-title").innerHTML = "Baby Step " + number;
+    document.getElementById("content-subtitle").innerHTML = babyStepData.name;
+    document.getElementById("content-main").innerHTML = babyStepData.content;
+  }
+
+  request.send();
+}
+
+function _getFriends(number) {
+  var request = new XMLHttpRequest();
+  request.overrideMimeType("application/json");
+  request.open("GET", "./baby-steps.json", true);
+
+  request.onload = function() {
+    var jsonResponse = JSON.parse(request.responseText);
+    var similarFriends = [];
+    for (var i = 0; i < jsonResponse.friends.length; i++) {
+      console.log('Evaluate', number, jsonResponse.friends[i].babyStep);
+      if (jsonResponse.friends[i].babyStep === number) {
+        similarFriends.push(jsonResponse.friends[i]);
+      }
+    }
+    document.getElementById("content-social").innerHTML = _compileSocialString(similarFriends, number);
+  }
+
+  request.send();
+}
+
+function _compileSocialString(names, babyStep) {
+  console.log("Name Length", names.length);
+  switch (names.length) {
+    case 0:
+      return '';
+    case 1:
+      return _getName(names[0]) + ' is also on Baby Step ' + babyStep;
+    case 2:
+      return _getName(names[0]) + ' and ' + _getName(names[1]) + ' are also in Baby Step ' + babyStep;
+    case 3:
+    default:
+      var difference = names.length - 2;
+      return _getName(names[0]) + ', ' + _getName(names[1]) + ', and '+ difference +' other ' + (difference === 1 ? 'friend' : 'friends') + ' are also in Baby Step ' + babyStep;
+  }
+}
+
+function _getName(name) {
+  return '<span class="content-social--name">' + name.firstName + ' ' + name.lastName + '</span>';
+}
+
+function _changeNavigation(number) {
+  document.getElementById("nav-" + currentStep).classList.remove('content-navigation-item--active');
+  document.getElementById("nav-" + number).classList.add('content-navigation-item--active');
+  currentStep = number;
+}
