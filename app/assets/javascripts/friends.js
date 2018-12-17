5c053479091214c16a2d46db02c87f6529283ce4
JonathonCisneros <cisneros.jonathon@gmail.com>
2018-12-17 11:07:14 -0800

    more updates

--- app/assets/javascripts/friends.js
+++ app/assets/javascripts/friends.js
@@ -1,18 +1,9 @@
-// $(".button").click( function() {
-//     $.getJSON("baby-steps.json", function(obj) {
-//         $.each(obj, function(key, value) {
-//             $(".babySteps").append("<p>" + value.firstName + "</p>");
-//         });
-//     });
-// });
-"use strict";
-
-let myRequest = new Request("./baby-steps.json");
+let myRequest = new Request("../baby-steps.json");
 
 fetch(myRequest)
     .then(function(resp){
         return resp.json();
     })
-    .then(function(things) {
-        console.log(things.friends);
+    .then(function(data) {
+        console.log(data.friends);
     });
