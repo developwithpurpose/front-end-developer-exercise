4926ac049d550692038d7c4d404515c92474c4b9
JonathonCisneros <cisneros.jonathon@gmail.com>
2018-12-17 10:55:12 -0800

    adding friends.js

---
+++ app/assets/javascripts/friends.js
@@ -0,0 +1,18 @@
+// $(".button").click( function() {
+//     $.getJSON("baby-steps.json", function(obj) {
+//         $.each(obj, function(key, value) {
+//             $(".babySteps").append("<p>" + value.firstName + "</p>");
+//         });
+//     });
+// });
+"use strict";
+
+let myRequest = new Request("./baby-steps.json");
+
+fetch(myRequest)
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data) {
+        console.log(data.friends);
+    });
