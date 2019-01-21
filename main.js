2fb947402f7383b9a89bf979d6578f8aaa168e1b
matt henley <mattbhenley@gmail.com>
2019-01-20 21:15:20 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function openStep(evt, babyStep) {
+    var i, tabcontent, tablinks, change;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(babyStep).style.display = "block";
+    evt.currentTarget.className += " active"
+}
+
+/* Attempt at Dynamic Content*/
+$("friends").click(function () {
+    $.ajax({
+        url: "baby-steps.json", success: function (result) {
+            $("#friends").html(result);
+        }
+    });
+});
