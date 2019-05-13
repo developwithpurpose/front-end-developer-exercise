5c477ea216e0d1c605bec88744dced5930a9f2c1
Kenneth Hanley <khanley@medhost.com>
2019-05-12 23:33:29 -0500

    Added context switching to the navigation panel

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,9 @@
+$(function() {;
+  $('.nav-tab').click(function () {
+    for(var i=0; i<7; i++) {
+      $('.baby-step-' + i).addClass('hide');
+    }
+
+    $('.baby-step-' + this.value).removeClass('hide');
+  });
+});
