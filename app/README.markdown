86bf25eed7411b78f9b8292d296077f097647e0a
Kirren Covey <kirrencovey@gmail.com>
2019-05-27 15:47:13 -0500

    Friends in baby step message prints below step description and changes on tab change.

--- app/README.markdown
+++ app/README.markdown
@@ -6,6 +6,6 @@ This is an outline of the steps I followed
 
 <!-- TO DO -->
 - Event listener on nav section - event bubbling to determine proper tab
-- Write JS function to toggle assigned classes on click so that correct info will be shown
+- Write JS to toggle assigned classes on click so that correct info will be shown
 - Write JS function to fetch friends and group by current step
 - Output matching friends to the DOM
--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,6 +1,41 @@
 
-// Add event listener to nav section to allow tab switching
+// Function to fetch friends info from .json
+let findFriends = (step) => {
+    fetch("../../../baby-steps.json")
+    .then((response) => response.json())
+    .then((response) => {
+        let friendsArray = Object.values(response)[0]
+        // Find friends on selected step
+        let matchingFriends = []
+        friendsArray.forEach(friend => {
+            if (friend.babyStep === parseInt(step)) {
+                matchingFriends.push(friend)
+            }
+        });
+        // Sort matching friends array alphabetically by last name
+        matchingFriends.sort((a, b) => (a.lastName > b.lastName) ? 1 : -1)
+        return matchingFriends
+    })
+    .then((matchingFriends) => {
+        // Create HTML for friends section
+        let html = ""
+        // build html string based on array length
+        if (matchingFriends.length === 0) {
+        } else if (matchingFriends.length === 1) {
+            html = `<a>${matchingFriends[0].firstName} ${matchingFriends[0].lastName}</a> is also in Baby Step ${step}`
+        } else if (matchingFriends.length === 2) {
+            html = `<a>${matchingFriends[0].firstName} ${matchingFriends[0].lastName}</a> and <a>${matchingFriends[1].firstName} ${matchingFriends[1].lastName}</a> are also in Baby Step ${step}`
+        } else if (matchingFriends.length > 2) {
+            html = `<a>${matchingFriends[0].firstName} ${matchingFriends[0].lastName}</a>, <a>${matchingFriends[1].firstName} ${matchingFriends[1].lastName}</a>, and ${matchingFriends.length - 2} other friends are also in Baby Step ${step}`
+        }
+        // Select friends section of DOM and print appropriate output
+        document.querySelector("#friends").innerHTML = html
+    })
+}
 
+
+
+// Event listener on nav section to allow tab switching
 document.querySelector("#nav-tabs").addEventListener("click", (e) => {
     // Find currently active tab and content
     let currentTab = document.querySelector(".active-nav")
@@ -12,15 +47,13 @@ document.querySelector("#nav-tabs").addEventListener("click", (e) => {
 
     // Find corresponding content section by id
     let selectedContent = document.querySelector(`#step-${stepNumber}`)
-    console.log(selectedTab.id.split("-")[1])
 
     // Toggle classes to switch active tab and content
     currentTab.classList.toggle("active-nav")
     currentContent.classList.toggle("active-content")
     selectedTab.classList.toggle("active-nav")
     selectedContent.classList.toggle("active-content")
+
+    // Find friends also on selected step and print to DOM
+    findFriends(stepNumber)
 })
-
-// Fetch friends info from .json
-
-// Populate DOM with friends on the selected step
