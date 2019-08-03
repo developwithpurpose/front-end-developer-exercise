047cdbef3d1e6f2c2c6a404b9654f01f18763102
Mike <mike.magnussen@lifeway.com>
2019-08-03 14:40:01 -0500

    getJson is functional

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,27 @@
 //Load JSON from 'baby-steps.json'
+var jqxhr = $.getJSON("http://localhost:8080/assets/javascripts/baby-steps.json", function (data) {
+    console.log(data);
 
+
+
+})
+    .done(function () {
+        console.log("second success");
+
+
+
+    })
+
+    .fail(function () {
+        console.log("error");
+    })
+    .always(function () {
+        console.log("complete");
+    });
+
+// Perform other work here ...
+
+// Set another completion function for the request above
+jqxhr;
 
 //Write to html
