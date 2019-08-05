43f6a4a04f9e8ed4bc7b9dd3226938ae81f36807
Mike <mike.magnussen@lifeway.com>
2019-08-04 21:18:51 -0500

    getJson/append functional in main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,27 +1,68 @@
 //Load JSON from 'baby-steps.json'
-var jqxhr = $.getJSON("http://localhost:8080/assets/javascripts/baby-steps.json", function (data) {
-    console.log(data);
 
+$(function () {
+    console.log("ready!");
 
+    $.getJSON('./assets/javascripts/baby-steps.json', function (data) {
+        console.log(data);
+        console.log(data.friends[0])
 
-})
-    .done(function () {
-        console.log("second success");
+            (Object.keys(data.friends) = 1 ? )
 
-
-
-    })
-
-    .fail(function () {
-        console.log("error");
-    })
-    .always(function () {
-        console.log("complete");
+        $("span.friends").append(`<p>${data.friends.map(x => JSON.stringify(x))}</p>`);
     });
+});
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+document.querySelector(".friends").onload = () => {
+    let displayResources = document.querySelector(".friends");
+    displayResources.textContent =
+        "Loading data from JSON source...";
+    fetch("./assets/javascripts/baby-steps.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result);
+
+            let output =
+                "<table><thead><tr><th>Name</th><th>Provider</th><th>URL</th></thead><tbody>";
+            for (let i in result) {
+                output +=
+                    "<tr><td>" +
+                    result[i].name +
+                    "</td><td>" +
+                    result[i].provider +
+                    "</td><td>" +
+                    result[i].url +
+                    "</td></tr>";
+            }
+            output += "</tbody></table>";
+            friends.innerHTML = output;
+        })
+        .catch((error) => {
+            console.log("reset client error-------", error)
+        });
+};
+*/
+//
 // Perform other work here ...
 
 // Set another completion function for the request above
-jqxhr;
+//jqxhr;
 
 //Write to html
