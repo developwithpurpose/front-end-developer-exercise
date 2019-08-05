2be30ed017fc754b538ee99b8b91e769ebb1a24e
Mike <mike.magnussen@lifeway.com>
2019-08-05 15:40:14 -0500

    friend counter and output finished

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -21,12 +21,24 @@ $(function () {
             console.log(currentStep);
             //console.log(data.friends[currentStep - 1]);
             let currentFriends = [];
-            let friendData = data.friends.filter(x => x.babyStep == currentStep);
+
+            let friendData = data.friends.filter(x => x.babyStep == currentStep).map((x, i) =>
+                (data.friends.filter(x => x.babyStep == currentStep).length == 1)
+                    ? currentFriends.push(`${x.firstName} ${x.lastName} is also in Baby Step ${currentStep}`)
+                    : (data.friends.filter(x => x.babyStep == currentStep).length == 2)
+                        ? currentFriends.push(`${x.firstName} ${x.lastName} and ${data.friends[i + 2].firstName} ${data.friends[i + 2].lastName} are also in Baby Step  ${currentStep}`)
+                        : (data.friends.filter(x => x.babyStep == currentStep).length == 3)
+                            ? currentFriends.push(`${x.firstName} ${x.lastName} and ${data.friends[i + 2].firstName} ${data.friends[i + 2].lastName} and 1 other friend are also in Baby Step  ${currentStep}`)
+                            : (data.friends.filter(x => x.babyStep == currentStep).length > 3)
+                                ? currentFriends.push(`${x.firstName} ${x.lastName} and ${data.friends[i + 2].firstName} ${data.friends[i + 2].lastName} and ${data.friends.filter(x => x.babyStep == currentStep).length - 1} other friends are also in Baby Step  ${currentStep}`)
+                                : '');
+
+            console.log(currentFriends);
             //console.log(friendData);
             console.log(friendData.length);
             //console.log(data.friends.map([]))
 
-            $("span.friends").html(`<p>${(friendData.length == 0) ? '' : (JSON.stringify(friendData))}</p>`);
+            $("span.friends").html(`<p>${(friendData.length == 0) ? '' : currentFriends[0]}</p>`);
         });
     }
 });
