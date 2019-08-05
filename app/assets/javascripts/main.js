52dc96279ac23a80e191ff992b64c034081901e0
Mike <mike.magnussen@lifeway.com>
2019-08-05 15:44:22 -0500

    tidy up main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,4 @@
 $(function () {
-    //console.log("ready!");
     let currentStep = 1;
     friendLoader();
     $(document).on('click', function () {
@@ -7,19 +6,13 @@ $(function () {
         console.log(currentStep);
         $('input:radio[name=radio-set]').change(function () {
             currentStep = this.value;
-            //console.log(currentStep);
-            // do stuff with currentStep
-            //Load JSON from 'baby-steps.json'
             friendLoader();
         });
     });
-    //Establish first button, and determine active radio button
 
     function friendLoader() {
         $.getJSON('./assets/javascripts/baby-steps.json', function (data) {
-            //console.log(data);
-            console.log(currentStep);
-            //console.log(data.friends[currentStep - 1]);
+
             let currentFriends = [];
 
             let friendData = data.friends.filter(x => x.babyStep == currentStep).map((x, i) =>
@@ -32,11 +25,6 @@ $(function () {
                             : (data.friends.filter(x => x.babyStep == currentStep).length > 3)
                                 ? currentFriends.push(`${x.firstName} ${x.lastName} and ${data.friends[i + 2].firstName} ${data.friends[i + 2].lastName} and ${data.friends.filter(x => x.babyStep == currentStep).length - 1} other friends are also in Baby Step  ${currentStep}`)
                                 : '');
-
-            console.log(currentFriends);
-            //console.log(friendData);
-            console.log(friendData.length);
-            //console.log(data.friends.map([]))
 
             $("span.friends").html(`<p>${(friendData.length == 0) ? '' : currentFriends[0]}</p>`);
         });
