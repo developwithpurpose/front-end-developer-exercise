1fa2b589ca1d73a0853884cbb41ff37577d19881
Laurel Herting <laurel.herting@gmail.com>
2019-08-07 10:55:09 -0600

    wip-update to newest react -v styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BabyStep1 from './components/BabyStep1';
 import BabyStep2 from './components/BabyStep2';
 import BabyStep3 from './components/BabyStep3';
@@ -9,34 +9,32 @@ import BabyStep7 from './components/BabyStep7';
 import logo from './assets/images/header.jpg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="Logo-image" alt="logo" />
-          <p>
-            Ramsey Solutions Rocks!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dave Ramsey's Baby Steps
-          </a>
-          <BabyStep1 />
-          <BabyStep2 />
-          <BabyStep3 />
-          <BabyStep4 />
-          <BabyStep5 />
-          <BabyStep6 />
-          <BabyStep7 />
-        </header>
-      </div>
-    );
-  }
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="Logo-image" alt="logo" />
+        <p>
+          Ramsey Solutions Rocks!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dave Ramsey's Baby Steps
+        </a>
+        <BabyStep1 />
+        <BabyStep2 />
+        <BabyStep3 />
+        <BabyStep4 />
+        <BabyStep5 />
+        <BabyStep6 />
+        <BabyStep7 />
+      </header>
+    </div>
+  );
+}
 
 export default App;
