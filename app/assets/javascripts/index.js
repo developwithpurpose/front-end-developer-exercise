ae2c97368aa4caba4ea2b5b7fc08d5319ad66561
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-09-19 00:51:44 -0500

    friends in step showing.

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,6 @@
-let selectStep = (function() {
+let selectStep = (function () {
   let selection;
-  return function(index) {
+  return function (index) {
     if (selection !== undefined) {
       toggleStep(selection, false);
     }
@@ -24,8 +24,8 @@ function toggleStep(index, toggle) {
     .attr("src", getBlueImage(index, toggle));
 }
 
-$(document).ready(function() {
-  $(".step-link").click(function(e) {
+$(document).ready(function () {
+  $(".step-link").click(function (e) {
     selectStep(
       $("#" + e.currentTarget.id)
         .parent()
@@ -41,5 +41,54 @@ $(document).ready(function() {
 function getBlueImage(index, blue) {
   return `./assets/images/icons/individual/icons_small_bs${index + 1}${
     blue ? "_blue" : ""
-  }.png`;
+    }.png`;
 }
+
+function getFriends() {
+  $.ajax({
+    url: "./baby-steps.json",
+    success: function (result) {
+      let friends = result.friends;
+      for (let i = 0; i < 7; i++) {
+        let filtered = friends.filter(function (friend) {
+          return friend.babyStep === i + 1;
+        });
+        $(".content-article .friends")
+          .eq(i)
+          .html(friendsInStep(i, filtered));
+      }
+    }
+  });
+}
+
+function friendsInStep(i, friends) {
+  if (friends.length >= 1) {
+    return (
+      "<a>" +
+      `${friends[0].firstName} ${friends[0].lastName}` +
+      "</a>" +
+      (friends.length > 1 ? (friends.length > 2 ? ", " : " and ") +
+        "<a>" + `${friends[1].firstName} ${friends[1].lastName}` +
+        "</a>" + (friends.length > 2 ? ", and " +
+          (friends.length - 2) + " other friend" +
+          (friends.length > 3 ? "s " : " ") : " ") + "are " : " is ") + "also in this Baby Step ");
+  }
+  return (
+    "<a>" + "Dont Quit!" + "</a>" + " " + "<span>" + "You can do this!" + "<span>"
+  );
+}
+
+$(document).ready(function () {
+  getFriends();
+  $(".step-link").click(function (e) {
+    selectStep(
+      $("#" + e.currentTarget.id)
+        .parent()
+        .parent()
+        .index()
+    );
+    return false;
+  });
+  $(".content-article").addClass("hidden");
+  selectStep(0);
+});
